9771359d72f3072665bf2d615023f7be36175194
Dreamwalker <aristojeff@gmail.com>
2019-11-06 22:01:25 +0900

    Update main_page.dart

--- lib/reminder_app/main_page.dart
+++ lib/reminder_app/main_page.dart
@@ -18,7 +18,66 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 34,
+            left: 24,
+            right: 24,
+            child: Container(
+              height: 38,
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.black,),
+                    onPressed: (){},
+                  ),
+                  
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red[300],
+      ),
+
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
