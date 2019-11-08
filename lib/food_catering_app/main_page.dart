3c138321debc47d0066a2be245201b5664ae5497
Dreamwalker <aristojeff@gmail.com>
2019-11-08 23:18:59 +0900

    Adds food catering app

---
+++ lib/food_catering_app/main_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class FoodCateringApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/food_catering_app/main_page.dart';
 import 'package:notebook_the_third_story/reminder_app/main_page.dart';
 
-void main() => runApp(ReminderApp());
+void main() => runApp(FoodCateringApp());
 
