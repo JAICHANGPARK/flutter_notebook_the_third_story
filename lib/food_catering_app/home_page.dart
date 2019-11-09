3f9f8685efb1ab132ed8e4bda81211ae21ad487b
Dreamwalker <aristojeff@gmail.com>
2019-11-09 18:54:33 +0900

    Update home_page.dart

--- lib/food_catering_app/home_page.dart
+++ lib/food_catering_app/home_page.dart
@@ -25,8 +25,48 @@ class _HomePageState extends State<HomePage> {
             right: 0,
             top: 0,
             child: Container(
-              height: 210,
-              decoration: BoxDecoration(color: Colors.blue),
+              height: 230,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://cdn.pixabay.com/photo/2019/10/27/14/08/vegetables-4581877__340.jpg"),
+                    fit: BoxFit.cover,
+                  )),
+              padding: EdgeInsets.only(right: 16, bottom: 16),
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: 24,
+                    left: 16,
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    bottom: 8,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          "Salads",
+                          style: TextStyle(color: Colors.white, fontSize: 28),
+                        ),
+                        Text(
+                          "48 items",
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           )
         ],
