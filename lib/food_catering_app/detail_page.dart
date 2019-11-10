5ff3569c3260f77293d85681d9175ca2cd89df9f
Dreamwalker <aristojeff@gmail.com>
2019-11-10 15:11:00 +0900

    Update detail_page.dart

--- lib/food_catering_app/detail_page.dart
+++ lib/food_catering_app/detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
@@ -33,6 +34,9 @@ class _DetailPageState extends State<DetailPage> {
             right: 0,
             bottom: 0,
             child: Container(
+              padding: EdgeInsets.only(left: 16, right: 16,
+              top: 16),
+
               height: MediaQuery.of(context).size.height / 2 - 60,
               decoration: BoxDecoration(
                   color: Colors.white,
@@ -40,6 +44,64 @@ class _DetailPageState extends State<DetailPage> {
                     topRight: Radius.circular(32),
                     topLeft: Radius.circular(32),
                   )),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Cezarre Original",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 120, top: 16),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 16,
+                        ),
+                        Text(
+                          "4.9",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.timer,
+                          color: Colors.grey,
+                          size: 16,
+                        ),
+                        Text(
+                          "15 mins",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.info_outline,
+                          color: Colors.grey,
+                          size: 16,
+                        ),
+                        Text(
+                          "Cheese",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 32),
+                    child: Text(
+                      "cheesy mayo sauce and mozzarella, tomatoes, green pepper, onion",
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           )
         ],
