a0a86d4be6a319ea1fd8c5ce15476088090208ec
Dreamwalker <aristojeff@gmail.com>
2019-11-11 21:39:16 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/flatmate_finder/main_page.dart';
 import 'package:notebook_the_third_story/food_catering_app/main_page.dart';
 import 'package:notebook_the_third_story/reminder_app/main_page.dart';
 
-void main() => runApp(FoodCateringApp());
+void main() => runApp(FlatmateFinder());
 
