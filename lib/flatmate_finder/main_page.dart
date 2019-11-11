0f0af01f4764571a138c4a6fa0d7e222753baabb
Dreamwalker <aristojeff@gmail.com>
2019-11-11 21:39:00 +0900

    Create main_page.dart

---
+++ lib/flatmate_finder/main_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/reminder_app/main_page.dart';
+
+
+class FlatmateFinder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
