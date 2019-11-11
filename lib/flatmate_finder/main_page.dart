2ca506cac46f85e4d08e495e82da1eda131a24cb
Dreamwalker <aristojeff@gmail.com>
2019-11-11 21:44:45 +0900

    Update main_page.dart

--- lib/flatmate_finder/main_page.dart
+++ lib/flatmate_finder/main_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notebook_the_third_story/reminder_app/main_page.dart';
 
-
 class FlatmateFinder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,7 +18,59 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 19,
+        child: Container(
+          height: 72,
+          padding: EdgeInsets.symmetric(horizontal: 16),
+//          decoration: BoxDecoration(color: Colors.blueGrey),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+                color: Colors.indigoAccent,
+              ),
+              IconButton(
+                icon: Icon(Icons.chat),
+                onPressed: () {},
+              ),
+              Container(
+                height: 52,
+                width: 52,
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.red[100],
+                          spreadRadius: 4,
+                          blurRadius: 6)
+                    ]),
+                child: Center(
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 38,
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite_border),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.account_circle),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
