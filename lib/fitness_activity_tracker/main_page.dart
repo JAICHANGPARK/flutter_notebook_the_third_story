8682c1a7d5cbe66227a6ae3202ee756091d0dfa5
Dreamwalker <aristojeff@gmail.com>
2019-11-14 20:55:44 +0900

    Adds

---
+++ lib/fitness_activity_tracker/main_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FitnessActivityTracker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/fitness_activity_tracker/main_page.dart';
 import 'package:notebook_the_third_story/flatmate_finder/main_page.dart';
 import 'package:notebook_the_third_story/food_catering_app/main_page.dart';
 import 'package:notebook_the_third_story/reminder_app/main_page.dart';
 
-void main() => runApp(FlatmateFinder());
+void main() => runApp(FitnessActivityTracker());
 
