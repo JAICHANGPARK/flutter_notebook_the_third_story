feb4fc8e845355cb639db02f53204bc5bfd4be72
Dreamwalker <aristojeff@gmail.com>
2019-11-14 21:09:50 +0900

    Update static_page.dart

--- lib/fitness_activity_tracker/static_page.dart
+++ lib/fitness_activity_tracker/static_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class StatisticsPage extends StatefulWidget {
   @override
   _StatisticsPageState createState() => _StatisticsPageState();
@@ -10,54 +9,43 @@ class _StatisticsPageState extends State<StatisticsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: Column(
-       children: <Widget>[
-         SafeArea(
-           top: true,
-           left: true,
-           right: true,
-           child: Row(
-             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-             children: <Widget>[
-               IconButton(
-                 onPressed: (){},
-                 icon: Icon(Icons.more_horiz),
-               ),
-               Text("Statistics"),
-               IconButton(
-                 icon: Icon(Icons.keyboard_arrow_down),
-                 onPressed: (){
-                   Navigator.of(context).pop();
-                 },
-               )
-             ],
-           ),
-         ),
-         Container()
-       ],
-     ),
+      body: Column(
+        children: <Widget>[
+          SafeArea(
+            top: true,
+            left: true,
+            right: true,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.more_horiz),
+                ),
+                Text("Statistics"),
+                IconButton(
+                  icon: Icon(Icons.keyboard_arrow_down),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 64),
+            child: Container(
+              height: MediaQuery.of(context).size.height - 136,
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(32),
+                    topLeft: Radius.circular(32),
+                  )),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
