ebe3b5e512039d71ac0c9699aeca3ff45c0b543c
Dreamwalker <aristojeff@gmail.com>
2019-11-17 19:52:05 +0900

    Update main_page.dart

--- lib/hotel_booking_app/main_page.dart
+++ lib/hotel_booking_app/main_page.dart
@@ -16,30 +16,58 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int currentIndex = 0;
+  Widget _buildBottomNavBar(){
+    return  BottomNavigationBar(
+      elevation: 9,
+      currentIndex: currentIndex,
+      type: BottomNavigationBarType.fixed,
+      onTap: (newValue) {
+        setState(() {
+          currentIndex = newValue;
+        });
+      },
+      selectedItemColor: Colors.greenAccent,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.search), title: Text("Explore")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border), title: Text("Trips")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle), title: Text("Profile")),
+      ],
+    );
 
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 9,
-        currentIndex: currentIndex,
-        type: BottomNavigationBarType.fixed,
-        onTap: (newValue) {
-          setState(() {
-            currentIndex = newValue;
-          });
-        },
-        selectedItemColor: Colors.greenAccent,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search), title: Text("Explore")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_border), title: Text("Trips")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle), title: Text("Profile")),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            
+          ],
+        ),
       ),
+      bottomNavigationBar:_buildBottomNavBar(),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
