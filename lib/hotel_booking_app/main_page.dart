6031a794b0219f8ffc99bad15ba2d1f838ffa9fc
Dreamwalker <aristojeff@gmail.com>
2019-11-17 19:59:27 +0900

    Update main_page.dart

--- lib/hotel_booking_app/main_page.dart
+++ lib/hotel_booking_app/main_page.dart
@@ -16,8 +16,10 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int currentIndex = 0;
-  Widget _buildBottomNavBar(){
-    return  BottomNavigationBar(
+  TextEditingController _textEditingController = TextEditingController();
+
+  Widget _buildBottomNavBar() {
+    return BottomNavigationBar(
       elevation: 9,
       currentIndex: currentIndex,
       type: BottomNavigationBarType.fixed,
@@ -37,37 +39,96 @@ class _MainPageState extends State<MainPage> {
             icon: Icon(Icons.account_circle), title: Text("Profile")),
       ],
     );
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            
+            SafeArea(
+              top: true,
+              left: true,
+              right: true,
+              child: Container(),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 4,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Hello Dreamwalker",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              "Find your hotel",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          height: 42,
+                          width: 42,
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(8)),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                      decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(16)),
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Center(
+                        child: TextField(
+                          controller: _textEditingController,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Search for hotels",
+                              icon: Icon(
+                                Icons.search,
+                                color: Colors.grey[400],
+                              ),
+                              hintStyle: TextStyle(
+                                color: Colors.grey[400],
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
-      bottomNavigationBar:_buildBottomNavBar(),
+      bottomNavigationBar: _buildBottomNavBar(),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
