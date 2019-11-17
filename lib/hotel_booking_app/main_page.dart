83c772f8fe21b7fb09b5dd979ebb2fcbc095659f
Dreamwalker <aristojeff@gmail.com>
2019-11-17 19:51:09 +0900

    Update main_page.dart

--- lib/hotel_booking_app/main_page.dart
+++ lib/hotel_booking_app/main_page.dart
@@ -15,11 +15,31 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 9,
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (newValue) {
+          setState(() {
+            currentIndex = newValue;
+          });
+        },
+        selectedItemColor: Colors.greenAccent,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), title: Text("Explore")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border), title: Text("Trips")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), title: Text("Profile")),
+        ],
+      ),
     );
   }
 }
-
