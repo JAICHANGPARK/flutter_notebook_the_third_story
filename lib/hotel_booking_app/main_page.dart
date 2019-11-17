8df93a3d4970954f7ec09c835aa1e01d2a260cb1
Dreamwalker <aristojeff@gmail.com>
2019-11-17 19:48:18 +0900

    Adds

--- lib/hotel_booking_app/main_page.dart
+++ lib/hotel_booking_app/main_page.dart
@@ -18,7 +18,7 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:notebook_the_third_story/fitness_activity_tracker/main_page.dart';
-import 'package:notebook_the_third_story/flatmate_finder/main_page.dart';
-import 'package:notebook_the_third_story/food_catering_app/main_page.dart';
-import 'package:notebook_the_third_story/reminder_app/main_page.dart';
 
-void main() => runApp(FitnessActivityTracker());
+import 'package:notebook_the_third_story/hotel_booking_app/main_page.dart';
+
+
+void main() => runApp(HotelBookingApp());
 
