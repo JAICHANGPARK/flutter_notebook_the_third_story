4fe4d95e8ae11739da0ba4f0080c33158929406b
Dreamwalker <aristojeff@gmail.com>
2019-11-19 22:19:51 +0900

    Update main_page.dart

--- lib/hotel_booking_app/main_page.dart
+++ lib/hotel_booking_app/main_page.dart
@@ -278,17 +278,228 @@ class _MainPageState extends State<MainPage> {
                               ),
                               Expanded(
                                 flex: 3,
-                                child: Placeholder(),
+                                child: Container(
+                                  margin: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 8),
+                                  decoration: BoxDecoration(
+                                    color: Colors.greenAccent,
+                                    borderRadius: BorderRadius.circular(16)
+                                  ),
+                                  child: Center(
+                                    child: RotatedBox(
+                                      child: Text("Book now",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18
+                                      ),), quarterTurns: 3,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ],
                           ),
                         ),
                         Container(
                           margin:
-                              EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                          EdgeInsets.only(left: 16, right: 16, bottom: 16),
                           height: MediaQuery.of(context).size.height / 5,
-                          child: Placeholder(),
-                        )
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(16),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.3),
+                                )
+                              ]),
+                          child: Row(
+                            children: <Widget>[
+                              Expanded(
+                                flex: 4,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(16),
+                                        bottomLeft: Radius.circular(16),
+                                      ),
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                          "https://cdn.pixabay.com/photo/2016/11/21/16/21/bed-1846251_960_720.jpg",
+                                        ),
+                                        fit: BoxFit.cover,
+                                      )),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 8,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                    CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        "The westin dhaka",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 8,
+                                      ),
+                                      Row(
+                                        children: <Widget>[
+                                          Icon(
+                                            Icons.location_on,
+                                            size: 16,
+                                          ),
+                                          Text(
+                                            " Kensington place",
+                                            style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 12),
+                                          )
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        height: 8,
+                                      ),
+                                      Text(
+                                        "\$180/night",
+                                        style: TextStyle(
+                                          color: Colors.indigo,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 8,
+                                      ),
+                                      Container(
+                                        height: 48,
+                                        child: Row(
+                                          children: <Widget>[
+                                            Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                margin: EdgeInsets.all(6),
+                                                decoration: BoxDecoration(
+                                                    borderRadius:
+                                                    BorderRadius.circular(
+                                                        4),
+                                                    color: Colors.white,
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                          color: Colors.black
+                                                              .withOpacity(.2),
+                                                          blurRadius: 3,
+                                                          spreadRadius: 1)
+                                                    ]),
+                                                child: Center(
+                                                    child: Icon(
+                                                      Icons.access_alarms,
+                                                      color: Colors.greenAccent,
+                                                    )),
+                                              ),
+                                            ),
+                                            Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                margin: EdgeInsets.all(6),
+                                                decoration: BoxDecoration(
+                                                    borderRadius:
+                                                    BorderRadius.circular(
+                                                        4),
+                                                    color: Colors.white,
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                          color: Colors.black
+                                                              .withOpacity(.2),
+                                                          blurRadius: 3,
+                                                          spreadRadius: 1)
+                                                    ]),
+                                                child: Center(
+                                                    child: Icon(
+                                                      Icons.access_alarms,
+                                                      color: Colors.greenAccent,
+                                                    )),
+                                              ),
+                                            ),
+                                            Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                margin: EdgeInsets.all(6),
+                                                decoration: BoxDecoration(
+                                                    borderRadius:
+                                                    BorderRadius.circular(
+                                                        4),
+                                                    color: Colors.white,
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                          color: Colors.black
+                                                              .withOpacity(.2),
+                                                          blurRadius: 3,
+                                                          spreadRadius: 1)
+                                                    ]),
+                                                child: Center(
+                                                    child: Icon(
+                                                      Icons.access_alarms,
+                                                      color: Colors.greenAccent,
+                                                    )),
+                                              ),
+                                            ),
+                                            Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                margin: EdgeInsets.all(6),
+                                                decoration: BoxDecoration(
+                                                    borderRadius:
+                                                    BorderRadius.circular(
+                                                        4),
+                                                    color: Colors.white,
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                          color: Colors.black
+                                                              .withOpacity(.2),
+                                                          blurRadius: 3,
+                                                          spreadRadius: 1)
+                                                    ]),
+                                                child: Center(
+                                                    child: Icon(
+                                                      Icons.access_alarms,
+                                                      color: Colors.greenAccent,
+                                                    )),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 3,
+                                child: Container(
+                                  margin: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 8),
+                                  decoration: BoxDecoration(
+                                      color: Colors.greenAccent,
+                                      borderRadius: BorderRadius.circular(16)
+                                  ),
+                                  child: Center(
+                                    child: RotatedBox(
+                                      child: Text("Book now",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18
+                                        ),), quarterTurns: 3,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
