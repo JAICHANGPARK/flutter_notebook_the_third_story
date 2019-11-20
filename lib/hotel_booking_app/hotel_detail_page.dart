e1763e55c84ccd43c9b2661757f932d95a531b09
Dreamwalker <aristojeff@gmail.com>
2019-11-20 23:09:26 +0900

    Create hotel_detail_page.dart

---
+++ lib/hotel_booking_app/hotel_detail_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HotelDetailPage extends StatefulWidget {
+  @override
+  _HotelDetailPageState createState() => _HotelDetailPageState();
+}
+
+class _HotelDetailPageState extends State<HotelDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
