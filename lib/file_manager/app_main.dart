54f9c4621a6fd6c12b8a71dd4b863904c8dae0a0
Dreamwalker <aristojeff@gmail.com>
2019-11-22 23:06:20 +0900

    Adds

--- lib/file_manager/app_main.dart
+++ lib/file_manager/app_main.dart
@@ -8,6 +8,29 @@ class AppMain extends StatefulWidget {
 class _AppMainState extends State<AppMain> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: MediaQuery.of(context).size.height / 3,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  topRight: Radius.circular(24),
+                  
+                )
+              ),
+            ),
+          )
+
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/file_manager/main_page.dart';
 
 import 'package:notebook_the_third_story/hotel_booking_app/main_page.dart';
 
 
-void main() => runApp(HotelBookingApp());
+void main() => runApp(FileManager());
 
