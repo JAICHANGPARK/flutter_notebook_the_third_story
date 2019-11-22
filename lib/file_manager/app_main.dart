e27ee4ae719e2771ad139cc2ee3637de8a684fe9
Dreamwalker <aristojeff@gmail.com>
2019-11-22 23:12:33 +0900

    Update app_main.dart

--- lib/file_manager/app_main.dart
+++ lib/file_manager/app_main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppMain extends StatefulWidget {
@@ -19,16 +20,59 @@ class _AppMainState extends State<AppMain> {
             top: MediaQuery.of(context).size.height / 3,
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(24),
-                  topRight: Radius.circular(24),
-                  
-                )
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(24),
+                  )),
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 5,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 16, right: 16),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text("Folders"),
+                              Text("See All"),
+                            ],
+                          ),
+                          Container(
+                            height: 160,
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              children: <Widget>[
+                                Container(
+                                  width: 160,
+                                  height: 160,
+                                  decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Placeholder(),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Placeholder(),
+                  )
+                ],
               ),
             ),
           )
-
         ],
       ),
     );
