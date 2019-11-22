dfe091bf161da994a90c07f37f14480fd373c717
Dreamwalker <aristojeff@gmail.com>
2019-11-22 23:03:35 +0900

    Adds

---
+++ lib/file_manager/app_main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppMain extends StatefulWidget {
+  @override
+  _AppMainState createState() => _AppMainState();
+}
+
+class _AppMainState extends State<AppMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/file_manager/main_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/file_manager/app_main.dart';
+
+class FileManager extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AppMain(),
+    );
+  }
+}
