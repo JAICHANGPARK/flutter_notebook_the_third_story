7768d9c501ac4e751e655279b5bf78acc7685b58
Dreamwalker <aristojeff@gmail.com>
2019-11-23 21:41:20 +0900

    Update app_main.dart

--- lib/file_manager/app_main.dart
+++ lib/file_manager/app_main.dart
@@ -7,12 +7,181 @@ class AppMain extends StatefulWidget {
 }
 
 class _AppMainState extends State<AppMain> {
+
+  Widget _buildFolderView(){
+    return Expanded(
+      flex: 6,
+      child: Padding(
+        padding:
+        const EdgeInsets.only(left: 16, right: 16, top: 24),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Folders",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  "See All",
+                  style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12),
+                ),
+              ],
+            ),
+            Container(
+              height: 160,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    margin: EdgeInsets.only(
+                        right: 16, bottom: 16, top: 16),
+                    width: 120,
+                    height: 160,
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.blue[100],
+                              blurRadius: 5,
+                              spreadRadius: 1,
+                              offset: Offset(0, 4))
+                        ],
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(16)),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment:
+                      MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 27,
+                          backgroundColor: Colors.blue[100],
+                        ),
+                        Text(
+                          "My Projects",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14),
+                        ),
+                        Text(
+                          "Created 20.02.95",
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.5),
+                            fontSize: 12,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                        right: 16, bottom: 16, top: 16),
+                    width: 120,
+                    height: 160,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border.all(
+                          color: Colors.grey,
+                        )),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment:
+                      MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 27,
+                          backgroundColor: Colors.blue[100],
+                        ),
+                        Text(
+                          "Dribbble",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14),
+                        ),
+                        Text(
+                          "Created 20.02.95",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                        right: 16, bottom: 16, top: 16),
+                    width: 120,
+                    height: 160,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border.all(
+                          color: Colors.grey,
+                        )),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment:
+                      MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 27,
+                          backgroundColor: Colors.blue[100],
+                        ),
+                        Text(
+                          "Behance",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14),
+                        ),
+                        Text(
+                          "Created 20.02.95",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
       body: Stack(
         children: <Widget>[
+          Positioned(
+            left: 16,
+            top: 32,
+            right: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height / 3.5,
+              child: Placeholder(),
+            ),
+          ),
           Positioned(
             left: 0,
             right: 0,
@@ -27,163 +196,7 @@ class _AppMainState extends State<AppMain> {
                   )),
               child: Column(
                 children: <Widget>[
-                  Expanded(
-                    flex: 6,
-                    child: Padding(
-                      padding:
-                          const EdgeInsets.only(left: 16, right: 16, top: 24),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(
-                                "Folders",
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                ),
-                              ),
-                              Text(
-                                "See All",
-                                style: TextStyle(
-                                    color: Colors.blueAccent,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 12),
-                              ),
-                            ],
-                          ),
-                          Container(
-                            height: 160,
-                            child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              shrinkWrap: true,
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.symmetric(vertical: 8),
-                                  margin: EdgeInsets.only(
-                                      right: 16, bottom: 16, top: 16),
-                                  width: 120,
-                                  height: 160,
-                                  decoration: BoxDecoration(
-                                      boxShadow: [
-                                        BoxShadow(
-                                            color: Colors.blue[100],
-                                            blurRadius: 5,
-                                            spreadRadius: 1,
-                                            offset: Offset(0, 4))
-                                      ],
-                                      color: Colors.blue,
-                                      borderRadius: BorderRadius.circular(16)),
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    children: <Widget>[
-                                      CircleAvatar(
-                                        radius: 27,
-                                        backgroundColor: Colors.blue[100],
-                                      ),
-                                      Text(
-                                        "My Projects",
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 14),
-                                      ),
-                                      Text(
-                                        "Created 20.02.95",
-                                        style: TextStyle(
-                                          color: Colors.white.withOpacity(0.5),
-                                          fontSize: 12,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.only(
-                                      right: 16, bottom: 16, top: 16),
-                                  width: 120,
-                                  height: 160,
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(16),
-                                      border: Border.all(
-                                        color: Colors.grey,
-                                      )),
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    children: <Widget>[
-                                      CircleAvatar(
-                                        radius: 27,
-                                        backgroundColor: Colors.blue[100],
-                                      ),
-                                      Text(
-                                        "Dribbble",
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 14),
-                                      ),
-                                      Text(
-                                        "Created 20.02.95",
-                                        style: TextStyle(
-                                          color: Colors.grey,
-                                          fontSize: 12,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.only(
-                                      right: 16, bottom: 16, top: 16),
-                                  width: 120,
-                                  height: 160,
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(16),
-                                      border: Border.all(
-                                        color: Colors.grey,
-                                      )),
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    children: <Widget>[
-                                      CircleAvatar(
-                                        radius: 27,
-                                        backgroundColor: Colors.blue[100],
-                                      ),
-                                      Text(
-                                        "Behance",
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 14),
-                                      ),
-                                      Text(
-                                        "Created 20.02.95",
-                                        style: TextStyle(
-                                          color: Colors.grey,
-                                          fontSize: 12,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
+                  _buildFolderView(),
                   Expanded(
                     flex: 6,
                     child: Placeholder(),
