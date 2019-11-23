b7b17e435c078221267fc0b946a04cddc5c5126d
Dreamwalker <aristojeff@gmail.com>
2019-11-23 21:53:43 +0900

    Update app_main.dart

--- lib/file_manager/app_main.dart
+++ lib/file_manager/app_main.dart
@@ -165,121 +165,163 @@ class _AppMainState extends State<AppMain> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            left: 16,
-            top: 32,
-            right: 16,
-            child: Container(
-              height: MediaQuery.of(context).size.height / 3.5,
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                left: 16,
+                top: 32,
+                right: 16,
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 3.5,
+                  child: Column(
                     children: <Widget>[
-                      IconButton(
-                        icon: Icon(
-                          Icons.menu,
-                          color: Colors.white,
-                        ),
-                        iconSize: 35,
-                        padding: EdgeInsets.zero,
-                        onPressed: () {},
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(
+                              Icons.menu,
+                              color: Colors.white,
+                            ),
+                            iconSize: 35,
+                            padding: EdgeInsets.zero,
+                            onPressed: () {},
+                          ),
+                          Badge(
+                            child: Icon(
+                              Icons.notifications_none,
+                              size: 32,
+                              color: Colors.white,
+                            ),
+                            position: BadgePosition.topRight(),
+                            padding: EdgeInsets.all(8),
+                          )
+                        ],
                       ),
-                      Badge(
-                        child: Icon(
-                          Icons.notifications_none,
-                          size: 32,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  "Hello",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                  ),
+                                ),
+                                Text(
+                                  "Dreamwalker....",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 42,
+                            width: 42,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(8),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://avatars2.githubusercontent.com/u/19484515?s=460&v=4"),
+                                  fit: BoxFit.cover),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Container(
+                        height: 52,
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
                           color: Colors.white,
+                          borderRadius: BorderRadius.circular(32),
                         ),
-                        position: BadgePosition.topRight(),
-                        padding: EdgeInsets.all(8),
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
+                        padding: EdgeInsets.only(left: 16, right: 16),
+                        child: Row(
                           children: <Widget>[
-                            Text(
-                              "Hello",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 24,
+                            Expanded(
+                              flex: 8,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintStyle:
+                                      TextStyle(color: Colors.grey, fontSize: 14),
+                                  hintText: "Search",
+                                  border: InputBorder.none,
+                                ),
                               ),
                             ),
-                            Text(
-                              "Dreamwalker....",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 24),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: VerticalDivider(
+                                thickness: 1,
+                                color: Colors.grey[400],
+                              ),
+                            ),
+                            Expanded(
+                              flex: 3,
+                              child: IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.search,
+                                  size: 32,
+                                  color: Colors.grey,
+                                ),
+                              ),
                             )
                           ],
                         ),
-                      ),
-                      Container(
-                        height: 42,
-                        width: 42,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(8),
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://avatars2.githubusercontent.com/u/19484515?s=460&v=4"),
-                              fit: BoxFit.cover),
-                        ),
                       )
                     ],
                   ),
-                  SizedBox(height: 16,),
-                  Container(
-                    height: 58,
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
+                ),
+              ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                top: MediaQuery.of(context).size.height / 3,
+                child: Container(
+                  decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(32)
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            top: MediaQuery.of(context).size.height / 3,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(24),
-                    topRight: Radius.circular(24),
-                  )),
-              child: Column(
-                children: <Widget>[
-                  _buildFolderView(),
-                  Expanded(
-                    flex: 6,
-                    child: Placeholder(),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24),
+                        topRight: Radius.circular(24),
+                      )),
+                  child: Column(
+                    children: <Widget>[
+                      _buildFolderView(),
+                      Expanded(
+                        flex: 6,
+                        child: Placeholder(),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Placeholder(),
+                      )
+                    ],
                   ),
-                  Expanded(
-                    flex: 2,
-                    child: Placeholder(),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
