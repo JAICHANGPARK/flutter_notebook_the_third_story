86e1e766c353781b109bf25d397ba1ff953e039a
Dreamwalker <aristojeff@gmail.com>
2019-11-25 23:42:19 +0900

    UPdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:notebook_the_third_story/travel_store_app/main_page.dart';
-
 
 void main() => runApp(TravelStoreApp());
 
--- lib/travel_store_app/main_page.dart
+++ lib/travel_store_app/main_page.dart
@@ -16,7 +16,61 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 240,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage("")
+                )
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: MediaQuery.of(context).size.height  - 240,
+            child: Container(
+              color: Colors.yellow[400],
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.brown
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
