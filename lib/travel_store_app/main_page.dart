2ba4782d4decd7f1a19ae1d65d95da3939b9b257
Dreamwalker <aristojeff@gmail.com>
2019-11-25 23:48:59 +0900

    Update main_page.dart

--- lib/travel_store_app/main_page.dart
+++ lib/travel_store_app/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class TravelStoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -26,9 +27,53 @@ class _MainPageState extends State<MainPage> {
             bottom: 240,
             child: Container(
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage("")
-                )
+                  image: DecorationImage(
+                image: NetworkImage(
+                    "https://cdn.pixabay.com/photo/2016/01/09/18/28/old-1130743_960_720.jpg"),
+                fit: BoxFit.cover,
+              )),
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    left: 16,
+                    top: 240,
+                    right: 0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Travel",
+                          style: TextStyle(
+                            color: Colors.yellow[500],
+                            fontSize: 42,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Store App",
+                          style: TextStyle(
+                            color: Colors.yellow[500],
+                            fontSize: 42,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8,),
+                        Text(
+                          "The original solo female travel blogger who",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                        Text("documents her globetrotting",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            )),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
           ),
@@ -36,7 +81,7 @@ class _MainPageState extends State<MainPage> {
             left: 0,
             right: 0,
             bottom: 0,
-            top: MediaQuery.of(context).size.height  - 240,
+            top: MediaQuery.of(context).size.height - 240,
             child: Container(
               color: Colors.yellow[400],
             ),
@@ -47,9 +92,16 @@ class _MainPageState extends State<MainPage> {
             bottom: 0,
             child: Container(
               height: 80,
-              decoration: BoxDecoration(
-                color: Colors.brown
-              ),
+              decoration: BoxDecoration(color: Colors.brown,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(24),
+                topLeft: Radius.circular(24)
+              )),
+              child: Center(child: Text("Skip", style: TextStyle(
+                fontSize: 22,
+                color: Colors.yellow,
+                fontWeight: FontWeight.bold,
+              ),)),
             ),
           )
         ],
@@ -57,20 +109,3 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
