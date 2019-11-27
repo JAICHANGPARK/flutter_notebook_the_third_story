08771e2b57c3f32eab89f92bedbbdc137930c620
Dreamwalker <aristojeff@gmail.com>
2019-11-27 23:19:49 +0900

    Update product_page.dart

--- lib/travel_store_app/product_page.dart
+++ lib/travel_store_app/product_page.dart
@@ -10,6 +10,28 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        textTheme: TextTheme(
+          title: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          )
+        ),
+        title: Text("Product"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.apps),
+            onPressed: (){},
+          )
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Colors.black,
+
+        ),
+      ),
 
     );
   }
