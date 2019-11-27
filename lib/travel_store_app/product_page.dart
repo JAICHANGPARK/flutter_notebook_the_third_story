5175906703d869d6bbc29eb616cf9778aa2c8674
Dreamwalker <aristojeff@gmail.com>
2019-11-27 23:16:02 +0900

    Create product_page.dart

---
+++ lib/travel_store_app/product_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class ProductPage extends StatefulWidget {
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
