3549d760a831c1f5c440a30483baf3b967dcb16d
Dreamwalker <aristojeff@gmail.com>
2019-11-28 23:34:30 +0900

    Update book_page.dart

--- lib/travel_store_app/book_page.dart
+++ lib/travel_store_app/book_page.dart
@@ -8,6 +8,59 @@ class BookPage extends StatefulWidget {
 class _BookPageState extends State<BookPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        textTheme: TextTheme(
+            title: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold)),
+        title: Text("Product"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.apps),
+            onPressed: () {},
+          )
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 9,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: 4,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.brown,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24))),
+              child: Center(
+                child: Text(
+                  "Pay",
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
   }
 }
