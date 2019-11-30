f2c20f1a41435403be81c272e0c98fad07514bb7
Dreamwalker <aristojeff@gmail.com>
2019-11-30 23:43:17 +0900

    Create main_page.dart

---
+++ lib/camera_shop_app/main_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/fitness_activity_tracker/main_page.dart';
+
+class CameraShopApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
