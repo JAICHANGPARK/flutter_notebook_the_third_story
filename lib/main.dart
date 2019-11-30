155c83a820fb325e3530c9b2139a826180004288
Dreamwalker <aristojeff@gmail.com>
2019-11-30 23:44:45 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notebook_the_third_story/travel_store_app/main_page.dart';
+import 'package:notebook_the_third_story/camera_shop_app/main_page.dart';
 
-void main() => runApp(TravelStoreApp());
+void main() => runApp(CameraShopApp());
 
