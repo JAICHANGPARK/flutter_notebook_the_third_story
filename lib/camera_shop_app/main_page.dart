9dc517297076e537d3be16cb2c9e80e2b0407b1c
Dreamwalker <aristojeff@gmail.com>
2019-11-30 23:58:33 +0900

    Update main_page.dart

--- lib/camera_shop_app/main_page.dart
+++ lib/camera_shop_app/main_page.dart
@@ -22,6 +22,7 @@ class _MainPageState extends State<MainPage> {
       backgroundColor: Colors.blue[50],
       body: Column(
         children: <Widget>[
+          SizedBox(height: 24,),
           Expanded(
             flex: 3,
             child: Padding(
@@ -55,14 +56,90 @@ class _MainPageState extends State<MainPage> {
           Expanded(
             flex: 2,
            child: Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             crossAxisAlignment: CrossAxisAlignment.center,
              children: <Widget>[
-               
+               Container(
+                 height: 42,
+                 width: 72,
+                 decoration: BoxDecoration(
+                   color: Colors.black,
+                    borderRadius: BorderRadius.circular(24)
+                 ),
+                 child: Center(
+                   child: Text("Newest",style: TextStyle(
+                     color: Colors.white
+                   ),),
+                 ),
+               ),
+               SizedBox(width: 8,),
+               Container(
+
+                 height: 42,
+                 width: 72,
+                 decoration: BoxDecoration(
+                     color: Colors.white.withOpacity(0.5),
+                     borderRadius: BorderRadius.circular(24)
+                 ),
+                 child: Center(
+                   child: Text("Oldest",),
+                 ),
+               ),
+               SizedBox(width: 8,),
+               Container(
+                 height: 42,
+                 width: 72,
+                 decoration: BoxDecoration(
+                     color: Colors.white.withOpacity(0.5),
+                     borderRadius: BorderRadius.circular(24)
+                 ),
+                 child: Center(
+                   child: Text("Price",),
+                 ),
+               )
              ],
            ),
           ),
           Expanded(
-            flex: 10,
-            child: Placeholder(),
+            flex: 14,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 48),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    width: 240,
+                    margin: EdgeInsets.all(24),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(24),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 3,
+                          spreadRadius: 1
+                        )
+                      ]
+                    ),
+                  ),
+                  Container(
+                    width: 240,
+                    margin: EdgeInsets.all(24),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              blurRadius: 3,
+                              spreadRadius: 1
+                          )
+                        ]
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           Expanded(
             flex: 2,
