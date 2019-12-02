31c87711a61ae683b7bf62ba7dee26b08967ffbb
Dreamwalker <aristojeff@gmail.com>
2019-12-02 21:09:27 +0900

    Adds

--- lib/camera_shop_app/detail_page.dart
+++ lib/camera_shop_app/detail_page.dart
@@ -12,7 +12,33 @@ class _DetailPageState extends State<DetailPage> {
       backgroundColor: Colors.blue[50],
       body: Column(
         children: <Widget>[
-          
+          Expanded(
+            flex: 3,
+            child: SafeArea(
+              top: true,
+              left: true,
+              right: true,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 42,
+                    width: 42,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 12,
+            child: Placeholder(),
+          )
         ],
       ),
     );
