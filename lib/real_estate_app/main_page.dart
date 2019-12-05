b1c105accced9647117e725773dc416c38599c3b
Dreamwalker <aristojeff@gmail.com>
2019-12-05 21:42:33 +0900

    Update main_page.dart

--- lib/real_estate_app/main_page.dart
+++ lib/real_estate_app/main_page.dart
@@ -114,25 +114,69 @@ class _MainPageState extends State<MainPage> {
               child: Column(
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.only(left: 16, right: 16),
+                    padding: const EdgeInsets.only(
+                        left: 16, right: 16, top: 8, bottom: 8),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        Text("Travel with us",
-                         style: TextStyle(
-                           color: Colors.black,
-                           fontWeight: FontWeight.bold,
-                           fontSize: 24
-                         ),),
-                        Text("See All", style: TextStyle(
-                          fontSize: 16
-                        ),),
+                        Text(
+                          "Travel with us",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        ),
+                        Text(
+                          "See All",
+                          style: TextStyle(fontSize: 16),
+                        ),
                       ],
                     ),
                   ),
                   Container(
-                    height: MediaQuery.of(context).size.height / 3.5,
-                    child: Placeholder(),
+                    height: MediaQuery.of(context).size.height / 3.8,
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          left: 16,
+                          top: 8,
+                          bottom: 16,
+                          right: 16,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://cdn.pixabay.com/photo/2014/12/27/14/37/living-room-581073_960_720.jpg"),
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          right: 4,
+                          top: 84,
+                          child: Container(
+                            height: 34,
+                            width: 34,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black.withOpacity(0.1),
+                                      blurRadius: 1,
+                                      spreadRadius: 2)
+                                ]),
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 18,
+                              color: Colors.grey[400],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                   Row(
                     children: <Widget>[
@@ -152,24 +196,20 @@ class _MainPageState extends State<MainPage> {
                           Text("Walk to Oak Park"),
                           Text("Bright Madison Street Loft"),
                         ],
-                      )
-                      ,
+                      ),
                       Container(
                         height: 34,
                         width: 100,
                         decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.1),
-                              blurRadius: 1,
-                              spreadRadius: 1
-                            )
-                          ]
-                        ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  blurRadius: 1,
+                                  spreadRadius: 1)
+                            ]),
                       )
-
                     ],
                   )
                 ],
@@ -185,24 +225,3 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
