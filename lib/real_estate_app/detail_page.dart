c03767ba2e0187358e418854a2a4964d641bdb7c
Dreamwalker <aristojeff@gmail.com>
2019-12-07 23:38:16 +0900

    Update detail_page.dart

--- lib/real_estate_app/detail_page.dart
+++ lib/real_estate_app/detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
@@ -8,6 +9,75 @@ class DetailPage extends StatefulWidget {
 class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 72,
+          decoration: BoxDecoration(
+            color: Colors.white
+          ),
+          padding: EdgeInsets.only(left: 24, right: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text("134\$",style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18
+                      ),),
+                      Text("Night", style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),)
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.star_border,size: 14),
+                      Icon(Icons.star_border,size: 14),
+                      Icon(Icons.star_border,size: 14),
+                      Icon(Icons.star_border,size: 14),
+                      Icon(Icons.star_border,size: 14),
+                    ],
+                  )
+                ],
+              ),
+              Container(
+                height: 72,
+                margin: EdgeInsets.all(8),
+                width: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  color: Colors.pink[900],
+                  borderRadius: BorderRadius.circular(8)
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
