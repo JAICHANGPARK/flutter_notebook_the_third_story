99ba4e33c04adc0a8e134a47b3730e1e798f347c
Dreamwalker <aristojeff@gmail.com>
2019-12-09 21:20:35 +0900

    Update profile_page.dart

--- lib/fittel_app/profile_page.dart
+++ lib/fittel_app/profile_page.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -10,6 +11,67 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          top: true,
+          left: true,
+          right: true,
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 0,
+                  right: 24,
+                  top: 24
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios, size: 18,),
+                      onPressed: (){
+                        print("back button pressed");
+                      },
+                    ),
+                    Badge(
+                      child: Icon(Icons.chat,),
+                      showBadge: true,
+                      badgeContent: Text("2",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12
+                      ),),
+                    )
+
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
