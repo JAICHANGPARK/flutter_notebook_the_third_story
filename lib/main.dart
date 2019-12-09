da494a5f21f06455630ac7491d6ac4bc2ba19790
Dreamwalker <aristojeff@gmail.com>
2019-12-09 21:17:46 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:notebook_the_third_story/camera_shop_app/main_page.dart';
-import 'package:notebook_the_third_story/real_estate_app/main_page.dart';
+import 'package:notebook_the_third_story/fittel_app/main_page.dart';
 
-void main() => runApp(RealEstateApp());
+
+void main() => runApp(FittelApp());
 
