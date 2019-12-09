2232cd8685854c6e4a7707f7dcbdc82a9495cfa6
Dreamwalker <aristojeff@gmail.com>
2019-12-09 21:20:56 +0900

    Update profile_page.dart

--- lib/fittel_app/profile_page.dart
+++ lib/fittel_app/profile_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
@@ -20,29 +18,29 @@ class _ProfilePageState extends State<ProfilePage> {
           child: Column(
             children: <Widget>[
               Padding(
-                padding: const EdgeInsets.only(
-                  left: 0,
-                  right: 24,
-                  top: 24
-                ),
+                padding: const EdgeInsets.only(left: 0, right: 24, top: 24),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     IconButton(
-                      icon: Icon(Icons.arrow_back_ios, size: 18,),
-                      onPressed: (){
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        size: 18,
+                      ),
+                      onPressed: () {
                         print("back button pressed");
                       },
                     ),
                     Badge(
-                      child: Icon(Icons.chat,),
+                      child: Icon(
+                        Icons.chat,
+                      ),
                       showBadge: true,
-                      badgeContent: Text("2",style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12
-                      ),),
+                      badgeContent: Text(
+                        "2",
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
                     )
-
                   ],
                 ),
               )
@@ -53,25 +51,3 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
