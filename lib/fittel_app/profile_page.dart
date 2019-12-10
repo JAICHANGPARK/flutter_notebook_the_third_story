1577dc123a46e791a4b2b4a6eb5e784301aaa1b9
Dreamwalker <aristojeff@gmail.com>
2019-12-10 21:22:28 +0900

    Update profile_page.dart

--- lib/fittel_app/profile_page.dart
+++ lib/fittel_app/profile_page.dart
@@ -10,6 +10,7 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       body: SingleChildScrollView(
         child: SafeArea(
           top: true,
@@ -17,155 +18,170 @@ class _ProfilePageState extends State<ProfilePage> {
           right: true,
           child: Column(
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(left: 0, right: 24, top: 24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Container(
+                height: MediaQuery.of(context).size.height / 1.3,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(32),
+                    bottomLeft: Radius.circular(32),
+                  )
+                ),
+                child: Column(
                   children: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        Icons.arrow_back_ios,
-                        size: 18,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 0, right: 24, top: 24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(
+                              Icons.arrow_back_ios,
+                              size: 18,
+                            ),
+                            onPressed: () {
+                              print("back button pressed");
+                            },
+                          ),
+                          Badge(
+                            child: Icon(
+                              Icons.chat,
+                            ),
+                            showBadge: true,
+                            badgeContent: Text(
+                              "2",
+                              style: TextStyle(color: Colors.white, fontSize: 12),
+                            ),
+                          )
+                        ],
                       ),
-                      onPressed: () {
-                        print("back button pressed");
-                      },
                     ),
-                    Badge(
-                      child: Icon(
-                        Icons.chat,
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Container(
+                      height: MediaQuery
+                          .of(context)
+                          .size
+                          .height / 3.5,
+                      child: Column(
+                        children: <Widget>[
+                          CircleAvatar(
+                            radius: 50,
+                            backgroundImage: NetworkImage(
+                                "https://avatars2.githubusercontent.com/u/19484515?s=460&v=4"),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Dreamwalker",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                                letterSpacing: 1.5),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Container(
+                            height: 42,
+                            margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                            width: MediaQuery
+                                .of(context)
+                                .size
+                                .width / 2.4,
+                            decoration: BoxDecoration(
+                                color: Colors.teal[200],
+                                borderRadius: BorderRadius.circular(8),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.teal[100],
+                                      spreadRadius: 1,
+                                      blurRadius: 2)
+                                ]),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  "Message",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 12,
+                                ),
+                                Icon(
+                                  Icons.chat_bubble_outline,
+                                  color: Colors.white,
+                                  size: 18,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                      showBadge: true,
-                      badgeContent: Text(
-                        "2",
-                        style: TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                    SizedBox(height: 16,),
+                    Container(
+                      height: MediaQuery
+                          .of(context)
+                          .size
+                          .height / 14,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text("Programs", style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16),),
+                                SizedBox(height: 8,),
+                                Text("Flutter", style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20
+                                ),),
+                              ],
+                            ),
+                          ),
+                          VerticalDivider(
+                            thickness: 1,
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text("Episode", style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16),),
+                                SizedBox(height: 8,),
+                                Text("295", style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20
+                                ),),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     )
                   ],
                 ),
               ),
-              SizedBox(
-                height: 24,
-              ),
-              Container(
-                height: MediaQuery
-                    .of(context)
-                    .size
-                    .height / 3.5,
-                child: Column(
-                  children: <Widget>[
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundImage: NetworkImage(
-                          "https://avatars2.githubusercontent.com/u/19484515?s=460&v=4"),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Dreamwalker",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
-                          letterSpacing: 1.5),
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Container(
-                      height: 42,
-                      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                      width: MediaQuery
-                          .of(context)
-                          .size
-                          .width / 2.4,
-                      decoration: BoxDecoration(
-                          color: Colors.teal[200],
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.teal[100],
-                                spreadRadius: 1,
-                                blurRadius: 2)
-                          ]),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            "Message",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 12,
-                          ),
-                          Icon(
-                            Icons.chat_bubble_outline,
-                            color: Colors.white,
-                            size: 18,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16,),
-              Container(
-                height: MediaQuery
-                    .of(context)
-                    .size
-                    .height / 14,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text("Programs", style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),),
-                          SizedBox(height: 8,),
-                          Text("Flutter", style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20
-                          ),),
-                        ],
-                      ),
-                    ),
-                    VerticalDivider(
-                      thickness: 1,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text("Episode", style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),),
-                          SizedBox(height: 8,),
-                          Text("295", style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20
-                          ),),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )
+
             ],
           ),
         ),
