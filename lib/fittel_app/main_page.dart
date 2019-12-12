a504678c25ebf7334b4ad1afca845ee733939966
Dreamwalker <aristojeff@gmail.com>
2019-12-12 21:36:31 +0900

    Update main_page.dart

--- lib/fittel_app/main_page.dart
+++ lib/fittel_app/main_page.dart
@@ -9,11 +9,12 @@ class FittelApp extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
       theme: ThemeData(
-        textTheme: GoogleFonts.lakkiReddyTextTheme(textTheme,).copyWith(
-         body1: GoogleFonts.oswald(
-           textStyle: textTheme.body1
-         )
-        )
+        textTheme: GoogleFonts.allertaTextTheme()
+//        textTheme: GoogleFonts.lakkiReddyTextTheme(textTheme,).copyWith(
+//         body1: GoogleFonts.oswald(
+//           textStyle: textTheme.body1
+//         )
+//        )
       ),
       home: ProfilePage(),
     );
