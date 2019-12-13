946024c03d3a346aba9300a85660288d5dc6504f
Dreamwalker <aristojeff@gmail.com>
2019-12-13 22:47:28 +0900

    Adds

--- lib/apartment_booking_app/main_page.dart
+++ lib/apartment_booking_app/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/apartment_booking_app/home_page.dart';
 
 class ApartmentBookingApp extends StatelessWidget {
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:notebook_the_third_story/fittel_app/main_page.dart';
+import 'package:notebook_the_third_story/apartment_booking_app/main_page.dart';
 
 
-void main() => runApp(FittelApp());
+void main() => runApp(ApartmentBookingApp());
 
