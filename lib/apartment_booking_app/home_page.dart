d467a9959e10bf5835e7aede1e0397c37df71c20
Dreamwalker <aristojeff@gmail.com>
2019-12-13 22:54:14 +0900

    Update home_page.dart

--- lib/apartment_booking_app/home_page.dart
+++ lib/apartment_booking_app/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -10,12 +9,46 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: Row(
+          children: <Widget>[
+            Icon(Icons.keyboard_arrow_left,
+            color: Colors.deepOrange,),
+            Text("Back", style: TextStyle(
+              color: Colors.white54
+            ),)
+          ],
+        ),
+        centerTitle: true,
+        title: Column(
+          children: <Widget>[
+            Text("Oslo, No"),
+            Text(
+              "10/9/2019 - 12/9/2019",
+              style: TextStyle(
+                  color: Colors.white.withOpacity(
+                    0.5,
+                  ),
+                  fontSize: 12),
+            )
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.filter_list,
+              color: Colors.deepOrange,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
       backgroundColor: const Color(0xff003041),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color(0xff003041),
-        onTap: (index){
-
-        },
+        onTap: (index) {},
         currentIndex: 0,
         elevation: 9,
         type: BottomNavigationBarType.fixed,
