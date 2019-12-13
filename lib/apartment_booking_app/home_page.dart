a70c1d43f05ea440e5c0e80fa9d270f67fd4f69b
Dreamwalker <aristojeff@gmail.com>
2019-12-13 22:50:10 +0900

    Update home_page.dart

--- lib/apartment_booking_app/home_page.dart
+++ lib/apartment_booking_app/home_page.dart
@@ -9,6 +9,35 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xff003041),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xff003041),
+        onTap: (index){
+
+        },
+        currentIndex: 0,
+        elevation: 9,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text("Search"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text("Search"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text("Search"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text("Search"),
+          ),
+        ],
+      ),
+    );
   }
 }
