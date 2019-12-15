77cf69ecf1c9b20583a2b7f6c6a577a0b3086d6a
Dreamwalker <aristojeff@gmail.com>
2019-12-15 19:44:23 +0900

    Update main_page.dart

--- lib/user_profile/main_page.dart
+++ lib/user_profile/main_page.dart
@@ -23,25 +23,74 @@ class _MainPageState extends State<MainPage> {
                     height: MediaQuery.of(context).size.height / 4.5,
                     child: Placeholder(),
                   ),
+                ),
+                Positioned(
+                  left: 24,
+                  top: MediaQuery.of(context).size.height / 4.5 - 28,
+                  child: Container(
+                    height: 84,
+                    width: 84,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 24,
+                  top: MediaQuery.of(context).size.height / 4.5 + 16,
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        height: 32,
+                        width: 32,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue),
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Container(
+                        height: 32,
+                        width: 32,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue),
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Container(
+                        height: 32,
+                        width: 32,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue),
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Container(
+                        height: 32,
+                        width: 32,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue),
+                      )
+                    ],
+                  ),
                 )
-
               ],
             ),
           ),
           Expanded(
             flex: 1,
             child: Container(
-              decoration: BoxDecoration(
-                color: Colors.teal
-              ),
+              decoration: BoxDecoration(color: Colors.teal),
               child: Center(
                 child: Text(
                   "Contact Dreamwalker",
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                  ),
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
             ),
