40da9cbc66a9bf038b1822b9579202c22d9a7438
Dreamwalker <aristojeff@gmail.com>
2019-12-17 23:48:24 +0900

    Update as_main_page.dart

--- lib/automotive_service/as_main_page.dart
+++ lib/automotive_service/as_main_page.dart
@@ -9,37 +9,66 @@ class _ASMainPageState extends State<ASMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 80,
-          padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Icon(Icons.home, size: 32,),
-                  Container(
-                    height: 3,
-                    width: 8,
-                    decoration: BoxDecoration(
-                      color: Colors.blue
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
+        body: Column(
+          children: <Widget>[],
         ),
-      )
-    );
+        bottomNavigationBar: BottomAppBar(
+          elevation: 12,
+          child: Container(
+            height: 72,
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.home,
+                      size: 32,
+                    ),
+                    Container(
+                      height: 3,
+                      width: 8,
+                      decoration: BoxDecoration(color: Colors.blue),
+                    )
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.settings,
+                      size: 32,
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.chat,
+                      size: 32,
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.chat_bubble_outline,
+                      size: 32,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
