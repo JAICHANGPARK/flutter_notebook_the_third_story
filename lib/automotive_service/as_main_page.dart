acf9f407a5e4f729ea750e6a4f7c429295c1762a
Dreamwalker <aristojeff@gmail.com>
2019-12-17 23:43:10 +0900

    Adds

---
+++ lib/automotive_service/as_main_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ASMainPage extends StatefulWidget {
+  @override
+  _ASMainPageState createState() => _ASMainPageState();
+}
+
+class _ASMainPageState extends State<ASMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/automotive_service/main_p.dart
+++ lib/automotive_service/main_p.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/automotive_service/as_main_page.dart';
 
 class AutomotiveServiceApp extends StatelessWidget {
   @override
