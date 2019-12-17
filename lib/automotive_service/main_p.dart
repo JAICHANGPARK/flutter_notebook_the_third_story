23e379b9f191d28dd0bed3daa37e47976255954a
Dreamwalker <aristojeff@gmail.com>
2019-12-17 23:42:29 +0900

    Create main_p.dart

---
+++ lib/automotive_service/main_p.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AutomotiveServiceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ASMainPage(),
+    );
+  }
+}
