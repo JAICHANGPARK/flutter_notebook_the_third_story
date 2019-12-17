a36f58f444cbcbb7efd5ff6f451e48faf8211b62
Dreamwalker <aristojeff@gmail.com>
2019-12-17 23:46:46 +0900

    Adds

--- lib/automotive_service/as_main_page.dart
+++ lib/automotive_service/as_main_page.dart
@@ -8,6 +8,38 @@ class ASMainPage extends StatefulWidget {
 class _ASMainPageState extends State<ASMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 80,
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.home, size: 32,),
+                  Container(
+                    height: 3,
+                    width: 8,
+                    decoration: BoxDecoration(
+                      color: Colors.blue
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notebook_the_third_story/apartment_booking_app/main_page.dart';
-import 'package:notebook_the_third_story/user_profile/user_main.dart';
+import 'package:notebook_the_third_story/automotive_service/main_p.dart';
 
-
-void main() => runApp(UserProfileApp());
+void main() => runApp(AutomotiveServiceApp());
 
