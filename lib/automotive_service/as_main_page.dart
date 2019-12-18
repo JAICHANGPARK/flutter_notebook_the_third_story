e5da85efbb94fc36044728e8cf3267afc427aff3
Dreamwalker <aristojeff@gmail.com>
2019-12-18 20:56:45 +0900

    Update as_main_page.dart

--- lib/automotive_service/as_main_page.dart
+++ lib/automotive_service/as_main_page.dart
@@ -23,12 +23,16 @@ class _ASMainPageState extends State<ASMainPage> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text("Hi, Dreamwalker",style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 22
-                      ),),
-                      CircleAvatar(backgroundColor: Colors.red,)
+                      Text(
+                        "Hi, Dreamwalker",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22),
+                      ),
+                      CircleAvatar(
+                        backgroundColor: Colors.red,
+                      )
                     ],
                   ),
                 ),
@@ -41,25 +45,22 @@ class _ASMainPageState extends State<ASMainPage> {
                     scrollDirection: Axis.horizontal,
                     children: <Widget>[
                       Container(
-                        margin: EdgeInsets.only(left: 0, right: 12, bottom: 16, top: 16),
+                        margin: EdgeInsets.only(
+                            left: 0, right: 12, bottom: 16, top: 16),
                         width: MediaQuery.of(context).size.width - 120,
-                        decoration: BoxDecoration(
-                          color: Colors.blue
-                        ),
+                        decoration: BoxDecoration(color: Colors.blue),
                       ),
                       Container(
-                        margin: EdgeInsets.only(left: 0, right: 12, bottom: 16, top: 16),
+                        margin: EdgeInsets.only(
+                            left: 0, right: 12, bottom: 16, top: 16),
                         width: MediaQuery.of(context).size.width - 120,
-                        decoration: BoxDecoration(
-                            color: Colors.blue
-                        ),
+                        decoration: BoxDecoration(color: Colors.blue),
                       ),
                       Container(
-                        margin: EdgeInsets.only(left: 0, right: 12, bottom: 16, top: 16),
+                        margin: EdgeInsets.only(
+                            left: 0, right: 12, bottom: 16, top: 16),
                         width: MediaQuery.of(context).size.width - 120,
-                        decoration: BoxDecoration(
-                            color: Colors.blue
-                        ),
+                        decoration: BoxDecoration(color: Colors.blue),
                       )
                     ],
                   ),
@@ -71,11 +72,14 @@ class _ASMainPageState extends State<ASMainPage> {
                   alignment: Alignment.centerLeft,
                   child: Padding(
                     padding: const EdgeInsets.only(left: 16),
-                    child: Text("Today's Appointment",style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ),),
+                    child: Text(
+                      "Today's Appointment",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -86,16 +90,12 @@ class _ASMainPageState extends State<ASMainPage> {
                     Container(
                       margin: EdgeInsets.only(left: 16, right: 16, bottom: 16),
                       height: MediaQuery.of(context).size.height / 3.5,
-                      decoration: BoxDecoration(
-                        color: Colors.black
-                      ),
+                      decoration: BoxDecoration(color: Colors.black),
                     ),
                     Container(
                       margin: EdgeInsets.only(left: 16, right: 16, bottom: 16),
                       height: MediaQuery.of(context).size.height / 3.5,
-                      decoration: BoxDecoration(
-                          color: Colors.black
-                      ),
+                      decoration: BoxDecoration(color: Colors.black),
                     )
                   ],
                 ),
