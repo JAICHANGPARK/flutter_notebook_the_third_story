aebdbf2c042ecf070bf0dfa2201e19a294a4c993
Dreamwalker <aristojeff@gmail.com>
2019-12-19 23:58:45 +0900

    Adds

---
+++ lib/food_application/main_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/camera_shop_app/main_page.dart';
+
+class FoodApplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:notebook_the_third_story/automotive_service/main_p.dart';
 
-void main() => runApp(AutomotiveServiceApp());
+import 'package:notebook_the_third_story/food_application/main_page.dart';
+
+void main() => runApp(FoodApplication());
 
