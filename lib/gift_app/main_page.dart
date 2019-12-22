fbc50d0035b17da199e5bce205706b4c27109c52
Dreamwalker <aristojeff@gmail.com>
2019-12-22 13:37:39 +0900

    Update main_page.dart

--- lib/gift_app/main_page.dart
+++ lib/gift_app/main_page.dart
@@ -11,6 +11,19 @@ class _MainPageState extends State<ShopMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Container(
+              color: Colors.teal,
+            ),
+          )
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: pageIndex,
         type: BottomNavigationBarType.fixed,
