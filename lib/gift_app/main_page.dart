31911734790495f328c5b1af774161efce5bef50
Dreamwalker <aristojeff@gmail.com>
2019-12-22 13:36:29 +0900

    Update main_page.dart

--- lib/gift_app/main_page.dart
+++ lib/gift_app/main_page.dart
@@ -7,21 +7,31 @@ class ShopMainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<ShopMainPage> {
+  int pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: pageIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        onTap: (newValue){
+          setState(() {
+            pageIndex = newValue;
+          });
+        },
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home,),
             title: Text("Home"),
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.home,),
-            title: Text("Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.add_shopping_cart,),
+            title: Text("Cart"),
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.home,),
-            title: Text("Home"),
-          ),  BottomNavigationBarItem(icon: Icon(Icons.home,),
-            title: Text("Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.archive,),
+            title: Text("Archive"),
+          ),  BottomNavigationBarItem(icon: Icon(Icons.share,),
+            title: Text("Share"),
           )
 
         ],
