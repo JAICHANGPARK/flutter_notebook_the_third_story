83d1d50be9981eab30641ee3f57987f3334dd8f3
Dreamwalker <aristojeff@gmail.com>
2019-12-22 13:31:11 +0900

    Create home_page.dart

---
+++ lib/gift_app/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GiftShopApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
