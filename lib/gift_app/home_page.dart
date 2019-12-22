ecf87b5ef292d3f8c69752892b7157a264fe2662
Dreamwalker <aristojeff@gmail.com>
2019-12-22 13:33:21 +0900

    Adds

--- lib/gift_app/home_page.dart
+++ lib/gift_app/home_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/gift_app/main_page.dart';
 
 class GiftShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      home: ShopMainPage(),
     );
   }
 }
---
+++ lib/gift_app/main_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class ShopMainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<ShopMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home,),
+            title: Text("Home"),
+          )
+        ],
+      ),
+    );
+  }
+}
