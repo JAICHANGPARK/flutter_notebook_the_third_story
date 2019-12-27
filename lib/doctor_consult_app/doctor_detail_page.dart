821d5f2f0d4befd1beff7df8d8ff883f46b353b8
Dreamwalker <aristojeff@gmail.com>
2019-12-27 14:30:02 +0900

    Update doctor_detail_page.dart

--- lib/doctor_consult_app/doctor_detail_page.dart
+++ lib/doctor_consult_app/doctor_detail_page.dart
@@ -4,7 +4,23 @@ class DoctorDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.black,
+          onPressed: () {},
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          )
+        ],
+      ),
     );
   }
 }
