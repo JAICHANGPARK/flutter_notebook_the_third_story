cbb4bff180507a396ee2359045fad32d5632de96
Dreamwalker <aristojeff@gmail.com>
2019-12-27 14:25:13 +0900

    Adds

---
+++ lib/doctor_consult_app/doctor_detail_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class DoctorDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/doctor_consult_app/enter_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DoctorConsultationApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DoctorDetailPage(),
+    );
+  }
+}
