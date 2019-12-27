586ba17cf1793e83c9fdda23b7a6b078aeacc08b
Dreamwalker <aristojeff@gmail.com>
2019-12-27 14:25:38 +0900

    Update enter_page.dart

--- lib/doctor_consult_app/enter_page.dart
+++ lib/doctor_consult_app/enter_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/doctor_consult_app/doctor_detail_page.dart';
 
 class DoctorConsultationApp extends StatelessWidget {
   @override
