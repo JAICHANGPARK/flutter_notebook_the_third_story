c56844d54737afafa4d087470d3485b1378f2285
Dreamwalker <aristojeff@gmail.com>
2019-12-29 23:09:59 +0900

    Create enter_page.dart

---
+++ lib/solor_system_uii/enter_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SolarSystemUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DetailPage(),
+    );
+  }
+}
