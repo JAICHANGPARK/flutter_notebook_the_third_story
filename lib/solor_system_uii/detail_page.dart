815f0eeae9a71d495a04802bdeac95de41459884
Dreamwalker <aristojeff@gmail.com>
2019-12-29 23:30:11 +0900

    Update detail_page.dart

--- lib/solor_system_uii/detail_page.dart
+++ lib/solor_system_uii/detail_page.dart
@@ -23,11 +23,11 @@ class _DetailPageState extends State<DetailPage> {
               child: Stack(
                 children: <Widget>[
                   Positioned(
-                    right: -160,
+                    right: -150,
                     top: 0,
                     bottom: 0,
                     child: Container(
-                      width: MediaQuery.of(context).size.height / 2.5,
+                      width: MediaQuery.of(context).size.height / 2.6,
                       decoration: BoxDecoration(
                           image: DecorationImage(
                               image: NetworkImage(
@@ -37,11 +37,10 @@ class _DetailPageState extends State<DetailPage> {
                   ),
                   Positioned(
                     left: 8,
-                    top: 24,
+                    top: 32,
                     bottom: 16,
                     child: Container(
                       width: MediaQuery.of(context).size.width / 2,
-                      color: Colors.blueGrey,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
@@ -64,12 +63,64 @@ class _DetailPageState extends State<DetailPage> {
                           ),
                           Text(
                             "Distance from sun",
-                            style: TextStyle(color: Colors.grey, fontSize: 12),
+                            style: TextStyle(color: Colors.grey, fontSize: 14),
                           ),
+                          SizedBox(height: 4,),
                           Text(
                             "148,377,282 MI KM",
                             style: TextStyle(
                               color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            "Radius",
+                            style: TextStyle(color: Colors.grey, fontSize: 14),
+                          ),
+                          SizedBox(height: 4,),
+                          Text(
+                            "3,390 kilometers",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            "Year",
+                            style: TextStyle(color: Colors.grey, fontSize: 14),
+                          ),
+                          SizedBox(height: 4,),
+                          Text(
+                            "678 Earth Days",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            "Planet type",
+                            style: TextStyle(color: Colors.grey, fontSize: 14),
+                          ),
+                          SizedBox(height: 4,),
+                          Text(
+                            "Terrestrial",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
                             ),
                           )
                         ],
@@ -85,3 +136,12 @@ class _DetailPageState extends State<DetailPage> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
