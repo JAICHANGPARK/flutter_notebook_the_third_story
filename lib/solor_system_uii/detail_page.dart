977a8814fd0c1be3b4ce41c6e44c4a08bda18e82
Dreamwalker <aristojeff@gmail.com>
2019-12-29 23:18:32 +0900

    Update detail_page.dart

--- lib/solor_system_uii/detail_page.dart
+++ lib/solor_system_uii/detail_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
   final String name;
-
   DetailPage(this.name);
 
   @override
@@ -12,6 +11,37 @@ class DetailPage extends StatefulWidget {
 class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body:  SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height / 2,
+              decoration: BoxDecoration(color: Colors.black),
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    right: -160,
+                    top: 0,
+                    bottom: 0,
+                    child: Container(
+                      width: MediaQuery.of(context).size.height / 2.5,
+                      decoration:BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage("https://ian.macky.net/pat/map/globes/venus-skin-256-fast.gif"),
+                          fit: BoxFit.contain
+                        )
+                      ),
+                    ),
+                  ),
+                  
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
