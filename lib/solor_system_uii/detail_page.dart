be174a012c2a41f454440a21e2833f1ebe0949c2
Dreamwalker <aristojeff@gmail.com>
2019-12-30 13:34:26 +0900

    Update detail_page.dart

--- lib/solor_system_uii/detail_page.dart
+++ lib/solor_system_uii/detail_page.dart
@@ -167,20 +167,20 @@ class _DetailPageState extends State<DetailPage> {
                       )
                     ],
                   ),
-                  SizedBox(height: 8,),
-                  Text("Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war and is often referred to as the 'Red Planet'.[15][16] The latter refers to the effect of the iron oxide prevalent on Mars' surface, which gives it a reddish appearance distinctive among the astronomical bodies visible to the naked eye.[17] Mars is a terrestrial planet with a thin atmosphere, having surface features reminiscent both of the impact craters of the Moon and the valleys, deserts, and polar ice caps of Earth.",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 11
-                  ),),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war and is often referred to as the 'Red Planet'.[15][16] The latter refers to the effect of the iron oxide prevalent on Mars' surface, which gives it a reddish appearance distinctive among the astronomical bodies visible to the naked eye.[17] Mars is a terrestrial planet with a thin atmosphere, having surface features reminiscent both of the impact craters of the Moon and the valleys, deserts, and polar ice caps of Earth.",
+                    style: TextStyle(color: Colors.white, fontSize: 11),
+                  ),
                   Align(
                     alignment: Alignment.centerRight,
-                    child: Text("Read more",style: TextStyle(
-                      color: Colors.orange,
-                      fontSize: 11
-                    ),),
+                    child: Text(
+                      "Read more",
+                      style: TextStyle(color: Colors.orange, fontSize: 11),
+                    ),
                   )
-
                 ],
               ),
             )
