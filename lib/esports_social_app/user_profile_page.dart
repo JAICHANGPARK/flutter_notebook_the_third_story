3ff64e97805212529728f98742420c55ce1cab2f
Dreamwalker <aristojeff@gmail.com>
2020-01-01 15:35:22 +0900

    Update user_profile_page.dart

--- lib/esports_social_app/user_profile_page.dart
+++ lib/esports_social_app/user_profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 
@@ -7,9 +8,12 @@ class UserProfilePage extends StatefulWidget {
 }
 
 class _UserProfilePageState extends State<UserProfilePage> {
+  Color appAccentColor = Color(0xff68FE9A); // 104 254 154 68 fe 9a
+  Color bgColor = Color(0xFF24272C); // 36 39 44 24 27 2c
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: bgColor,
       body: Stack(
         children: <Widget>[
           Positioned(
@@ -33,8 +37,74 @@ class _UserProfilePageState extends State<UserProfilePage> {
             bottom: 0,
             child: Placeholder(),
           ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 80,
+
+              padding: EdgeInsets.only(left: 16, right: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  FloatingActionButton(
+                    child: Icon(Icons.add,color: Colors.black,
+                    size: 28,),
+                    backgroundColor: appAccentColor,
+                    onPressed: () {},
+                  ),
+                  Container(
+                    height: 52,
+                    width: MediaQuery.of(context).size.width - 120,
+                    padding: EdgeInsets.only(right: 24,left: 24),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Badge(
+                          child: Icon(Icons.home,color: Colors.white,),
+                          showBadge: false,
+                        ),
+                        Badge(
+                          child: Icon(Icons.search,color: Colors.white,),
+                          showBadge: false,
+                        ),
+                        Badge(
+                          child: Icon(Icons.favorite_border,color: Colors.white,),
+                          showBadge: true,
+                          badgeColor: appAccentColor,
+                          badgeContent: Text("2"),
+                        ),
+                        Badge(
+                          child: Icon(Icons.mail_outline,color: Colors.white,),
+                          showBadge: true,
+                          badgeColor: appAccentColor,
+                          badgeContent: Text("3"),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
