daafdd7f29448a71fb709751b608634161bfc91d
Dreamwalker <aristojeff@gmail.com>
2020-01-01 15:25:15 +0900

    Adds

--- lib/esports_social_app/user_profile_page.dart
+++ lib/esports_social_app/user_profile_page.dart
@@ -9,6 +9,32 @@ class UserProfilePage extends StatefulWidget {
 class _UserProfilePageState extends State<UserProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: MediaQuery.of(context).size.height / 2,
+            child: Placeholder(),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: MediaQuery.of(context).size.height / 2,
+            bottom: MediaQuery.of(context).size.height / 2.8,
+            child: Placeholder(),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top:  MediaQuery.of(context).size.height / 1.55,
+            bottom: 0,
+            child: Placeholder(),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notebook_the_third_story/solor_system_uii/enter_page.dart';
+import 'package:notebook_the_third_story/esports_social_app/page_importer.dart';
 
-void main() => runApp(SolarSystemUI());
+void main() => runApp(EsportSocialMediaApp());
 
