2e78cf7e21f480732e802fe2ed7e731c7d281fee
Dreamwalker <aristojeff@gmail.com>
2020-01-04 14:33:13 +0900

    Update importer

--- lib/esports_social_app/home_page.dart
+++ lib/esports_social_app/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 
@@ -6,9 +7,103 @@ class ESportMainPage extends StatefulWidget {
   _ESportMainPageState createState() => _ESportMainPageState();
 }
 
-class _ESportMainPageState extends State<ESportMainPage> {
+class _ESportMainPageState extends State<ESportMainPage> with SingleTickerProviderStateMixin{
+  TabController _tabController;
+  Color appAccentColor = Color(0xff68FE9A); // 104 254 154 68 fe 9a
+  Color bgColor = Color(0xFF24272C); // 36 39 44 24 27 2c
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _tabController = TabController(vsync: this, length: 5);
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Stack(
+        children: <Widget>[
+          //Bottom Navigation Bar
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 80,
+              padding: EdgeInsets.only(left: 16, right: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  FloatingActionButton(
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.black,
+                      size: 28,
+                    ),
+                    backgroundColor: appAccentColor,
+                    onPressed: () {},
+                  ),
+                  Container(
+                    height: 52,
+                    width: MediaQuery.of(context).size.width - 120,
+                    padding: EdgeInsets.only(right: 24, left: 24),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        InkWell(
+                          onTap: () {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => ESportMainPage()));
+                          },
+                          child: Badge(
+                            child: Icon(
+                              Icons.home,
+                              color: Colors.white,
+                            ),
+                            showBadge: false,
+                          ),
+                        ),
+                        Badge(
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                          showBadge: false,
+                        ),
+                        Badge(
+                          child: Icon(
+                            Icons.favorite_border,
+                            color: Colors.white,
+                          ),
+                          showBadge: true,
+                          badgeColor: appAccentColor,
+                          badgeContent: Text("2"),
+                        ),
+                        Badge(
+                          child: Icon(
+                            Icons.mail_outline,
+                            color: Colors.white,
+                          ),
+                          showBadge: true,
+                          badgeColor: appAccentColor,
+                          badgeContent: Text("3"),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/esports_social_app/page_importer.dart
+++ lib/esports_social_app/page_importer.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/esports_social_app/home_page.dart';
 import 'package:notebook_the_third_story/esports_social_app/user_profile_page.dart';
 
 class EsportSocialMediaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: UserProfilePage(),
+      home: ESportMainPage(),
     );
   }
 }
