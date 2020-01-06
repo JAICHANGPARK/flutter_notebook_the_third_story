2923c65388fa3ea2b43dcc7bd340facba8842dc8
Dreamwalker <aristojeff@gmail.com>
2020-01-06 22:02:13 +0900

    Update main_page.dart

--- lib/news_feed/main_page.dart
+++ lib/news_feed/main_page.dart
@@ -23,7 +23,6 @@ class _MainPageState extends State<MainPage> {
         fit: StackFit.expand,
         overflow: Overflow.visible,
         children: <Widget>[
-
           Positioned(
             left: 0,
             right: 0,
@@ -34,63 +33,51 @@ class _MainPageState extends State<MainPage> {
               children: <Widget>[
                 Container(
                   height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width /2.3,
+                  width: MediaQuery.of(context).size.width / 2.3,
                   child: ListView(
-                    children: List.generate(10, (index){
-                      return Container(
-                        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                        height: MediaQuery.of(context).size.height / 3,
-                        decoration: BoxDecoration(
-                          color: Colors.blue
-                        ),
-                      );
-                    }).toList()
-                  ),
+                      children: List.generate(10, (index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                      height: MediaQuery.of(context).size.height / 3,
+                      decoration: BoxDecoration(color: Colors.blue),
+                    );
+                  }).toList()),
                 ),
                 Container(
                   height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width /2.3,
+                  width: MediaQuery.of(context).size.width / 2.3,
                   child: ListView(
-                      children: List.generate(10, (index){
-                        return Container(
-                          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                          height: MediaQuery.of(context).size.height / 3.5,
-                          decoration: BoxDecoration(
-                              color: Colors.red
-                          ),
-                        );
-                      }).toList()
-                  ),
+                      children: List.generate(10, (index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                      height: MediaQuery.of(context).size.height / 3.5,
+                      decoration: BoxDecoration(color: Colors.red),
+                    );
+                  }).toList()),
                 ),
                 Container(
                   height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width /2.3,
+                  width: MediaQuery.of(context).size.width / 2.3,
                   child: ListView(
-                      children: List.generate(10, (index){
-                        return Container(
-                          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                          height: MediaQuery.of(context).size.height / 4,
-                          decoration: BoxDecoration(
-                              color: Colors.green
-                          ),
-                        );
-                      }).toList()
-                  ),
+                      children: List.generate(10, (index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                      height: MediaQuery.of(context).size.height / 4,
+                      decoration: BoxDecoration(color: Colors.green),
+                    );
+                  }).toList()),
                 ),
                 Container(
                   height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width /2.3,
+                  width: MediaQuery.of(context).size.width / 2.3,
                   child: ListView(
-                      children: List.generate(10, (index){
-                        return Container(
-                          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                          height: MediaQuery.of(context).size.height / 4,
-                          decoration: BoxDecoration(
-                              color: Colors.yellow
-                          ),
-                        );
-                      }).toList()
-                  ),
+                      children: List.generate(10, (index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                      height: MediaQuery.of(context).size.height / 4,
+                      decoration: BoxDecoration(color: Colors.yellow),
+                    );
+                  }).toList()),
                 ),
               ],
             ),
@@ -100,50 +87,46 @@ class _MainPageState extends State<MainPage> {
             right: 0,
             top: 0,
             child: Container(
+              padding: EdgeInsets.only(top: 24),
               height: MediaQuery.of(context).size.height / 4.5,
               decoration: BoxDecoration(
                 color: Colors.white.withOpacity(0.9),
               ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Stories",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
-
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.view_carousel,
-        color: Colors.white,),
-        backgroundColor: Colors.black, onPressed: () {  },
-
+        child: Icon(
+          Icons.view_carousel,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.black,
+        onPressed: () {},
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
