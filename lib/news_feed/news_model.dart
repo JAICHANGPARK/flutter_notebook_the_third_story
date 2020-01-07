4f2dddae9df1c105e53dae350d54107330f60da1
Dreamwalker <aristojeff@gmail.com>
2020-01-07 22:04:43 +0900

    Create news_model.dart

---
+++ lib/news_feed/news_model.dart
@@ -0,0 +1,26 @@
+/**
+ *
+ * "status": "ok",
+    "totalResults": 3180,
+    -"articles": [
+    -{
+    -"source": {
+    "id": "business-insider",
+    "name": "Business Insider"
+    },
+    "author": "Trista Kelley",
+    "title": "10 things you need to know before the opening bell",
+    "description": "Here's what you need to know. 1. Top US allies are abandoning Trump and warning ISIS will benefit following the assassination of an Iranian general. A spokesperson for British Prime Minister Boris Johnson said the US president's recent threat to target \"Irani…",
+    "url": "https://www.businessinsider.com/opening-bell-january-7-2020-2020-1",
+    "urlToImage": "https://image.businessinsider.com/5e147a9b855cc23ccb77e5f5?width=1200&format=jpeg",
+    "publishedAt": "2020-01-07T12:35:40Z",
+    "content": "Here's what you need to know. \r\n1. Top US allies are abandoning Trump and warning ISIS will benefit following the assassination of an Iranian general. A spokesperson for British Prime Minister Boris Johnson said the US president's recent threat to target \"Ira… [+2031 chars]"
+    },
+    -{
+ */
+
+class News{
+  String status;
+  int totalResults;
+
+}
