291c41de956f7b6198a79c6f6dcaf396bf3673ab
Dreamwalker <aristojeff@gmail.com>
2020-01-10 22:19:24 +0900

    Update main_page.dart

--- lib/mobile_shop_app/main_page.dart
+++ lib/mobile_shop_app/main_page.dart
@@ -228,7 +228,6 @@ class _MainPageState extends State<MainPage> {
                           Container(
                             margin: EdgeInsets.only(top: 4, right: 12),
                             width: MediaQuery.of(context).size.width / 2,
-
                             child: Column(
                               children: <Widget>[
                                 Expanded(
@@ -258,7 +257,16 @@ class _MainPageState extends State<MainPage> {
                                       Expanded(
                                         flex: 1,
                                         child: Container(
-                                          color: Colors.green,
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.only(
+                                              topRight: Radius.circular(8),
+                                              bottomRight: Radius.circular(8),
+                                            ),
+                                            image: DecorationImage(
+                                              image: NetworkImage("https://cdn.pixabay.com/photo/2016/03/09/10/23/model-1246028__340.jpg"),
+                                              fit: BoxFit.cover,
+                                            )
+                                          ),
                                         ),
                                       )
                                     ],
@@ -295,8 +303,76 @@ class _MainPageState extends State<MainPage> {
                           Container(
                             margin: EdgeInsets.only(top: 4, right: 12),
                             width: MediaQuery.of(context).size.width / 2,
-                            decoration: BoxDecoration(
-                                color: Colors.blue
+                            child: Column(
+                              children: <Widget>[
+                                Expanded(
+                                  flex: 6,
+                                  child: Row(
+                                    children: <Widget>[
+                                      Expanded(
+                                        flex: 1,
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.only(
+                                                topLeft: Radius.circular(8),
+                                                bottomLeft: Radius.circular(8),
+                                              ),
+                                              image: DecorationImage(
+                                                  image: NetworkImage("https://cdn.pixabay.com/photo/2016/11/21/16/01/attractive-1846127__340.jpg"),
+                                                  fit: BoxFit.cover
+                                              )
+                                          ),
+                                        ),
+                                      ),
+                                      VerticalDivider(
+                                        color: Colors.white,
+                                        width: 2,
+                                        thickness: 2,
+                                      ),
+                                      Expanded(
+                                        flex: 1,
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.only(
+                                                topRight: Radius.circular(8),
+                                                bottomRight: Radius.circular(8),
+                                              ),
+                                              image: DecorationImage(
+                                                image: NetworkImage("https://cdn.pixabay.com/photo/2016/03/09/10/23/model-1246028__340.jpg"),
+                                                fit: BoxFit.cover,
+                                              )
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 3,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text("Fleece Clothing", style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold
+                                      ),),
+                                      SizedBox(height: 4,),
+                                      Row(
+                                        children: <Widget>[
+                                          CircleAvatar(radius: 13, backgroundImage:
+                                          NetworkImage(dreamwalkerImg),),
+                                          SizedBox(width: 4,),
+                                          Text("Dreamwalker",style: TextStyle(
+                                              color: Colors.grey,
+                                              fontSize: 10
+                                          ),)
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ],
