6f4e655e121b9696a3b560b9abde582127b1f9a2
Dreamwalker <aristojeff@gmail.com>
2020-01-12 18:48:29 +0900

    Update main_page.dart

--- lib/e_commerce_app/main_page.dart
+++ lib/e_commerce_app/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/note_utils.dart';
 
 class EcommerceApp extends StatelessWidget {
   @override
@@ -20,71 +21,146 @@ class _CartPageState extends State<CartPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            top: MediaQuery.of(context).size.height / 3.6,
-            child: Container(
-              decoration: BoxDecoration(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: MediaQuery.of(context).size.height / 3.6,
+              child: Container(
+                decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(32),
-                      topRight: Radius.circular(32))),
-            ),
-          ),
-          Positioned(
-            left: 24,
-            right: 24,
-            bottom: 16,
-            child: Container(
-              height: MediaQuery.of(context).size.height / 8,
-              decoration: BoxDecoration(
-                  color: Color(0xff0F2739),
-                  borderRadius: BorderRadius.circular(64)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(
-                    "Cancel",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Container(
-                    height: 72,
-                    width: 72,
-                    decoration: BoxDecoration(
-                        color: Colors.red[500].withOpacity(0.4),
-                        shape: BoxShape.circle),
-                    margin: EdgeInsets.all(4),
-                    padding: EdgeInsets.all(6),
-                    child: CircleAvatar(
-                      backgroundColor: Colors.red,
-                      child: Text(
-                        "\$",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 21),
-                      ),
-                    ),
-                  ),
-                  Text(
-                    "Pay Now",
-                    style: TextStyle(
-                        color: Colors.orangeAccent,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
+                      topRight: Radius.circular(32)),
+                ),
               ),
             ),
-          )
-        ],
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: EdgeInsets.only(left: 24, right: 24),
+                height: MediaQuery.of(context).size.height / 3,
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Container(
+                            height: 38,
+                            width: 38,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Center(
+                              child: Icon(Icons.keyboard_arrow_left),
+                            ),
+                          ),
+                          Text(
+                            "Cart",
+                            style: TextStyle(
+                                color: Color(0xff0F2739),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 26),
+                          ),
+                          CircleAvatar(
+                            radius: 24,
+                            backgroundImage: NetworkImage(dreamwalkerImg),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              margin: EdgeInsets.only(
+                                  right: 8, top: 32, bottom: 32),
+                              decoration: BoxDecoration(color: Colors.white),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              margin: EdgeInsets.only(
+                                  right: 8, top: 32, bottom: 32),
+                              decoration: BoxDecoration(
+                                color: const Color(0xff0F2739),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Placeholder(),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 24,
+              right: 24,
+              bottom: 16,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 8,
+                decoration: BoxDecoration(
+                    color: const Color(0xff0F2739),
+                    borderRadius: BorderRadius.circular(64)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      "Cancel",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      height: 72,
+                      width: 72,
+                      decoration: BoxDecoration(
+                          color: Colors.red[500].withOpacity(0.4),
+                          shape: BoxShape.circle),
+                      margin: EdgeInsets.all(4),
+                      padding: EdgeInsets.all(6),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.red,
+                        child: Text(
+                          "\$",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 21),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Pay Now",
+                      style: TextStyle(
+                          color: Colors.orangeAccent,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
