45dc6bb9723ba8aad0281da4302c4c6a795dbb46
Dreamwalker <aristojeff@gmail.com>
2020-01-14 21:01:23 +0900

    Adds

--- lib/airtime_app/main_page.dart
+++ lib/airtime_app/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/airtime_app/main_view.dart';
 
 class AirtimeApp extends StatelessWidget {
   @override
--- lib/airtime_app/main_view.dart
+++ lib/airtime_app/main_view.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class AirtimeMainPage extends StatefulWidget {
+  @override
+  _AirtimeMainPageState createState() => _AirtimeMainPageState();
+}
+
+class _AirtimeMainPageState extends State<AirtimeMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
