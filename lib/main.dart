e357a2fee9f7fdd7e264118f969d00d4884adc23
Dreamwalker <aristojeff@gmail.com>
2020-01-17 23:15:09 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notebook_the_third_story/airtime_app/main_page.dart';
+import 'package:notebook_the_third_story/bookstore_app/book_store_app.dart';
 
 
-void main() => runApp(AirtimeApp());
+void main() => runApp(BookStoreApp());
 
