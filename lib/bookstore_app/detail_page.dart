ea58034014ee89e0d0e828d69107324003f0eda3
Dreamwalker <aristojeff@gmail.com>
2020-01-21 21:14:10 +0900

    Update detail_page.dart

--- lib/bookstore_app/detail_page.dart
+++ lib/bookstore_app/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/note_utils.dart';
 
 
 class DetailPage extends StatefulWidget {
@@ -31,6 +32,48 @@ class _DetailPageState extends State<DetailPage> {
               ),
             ),
           ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2.2,
+            left: 24,right: 16,
+            bottom: 0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Flutter Live Coding Book", style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22
+                ),),SizedBox(height: 8),
+                Text("Dreamwalker", style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22
+                ),),
+                SizedBox(height: 16),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star,color: Colors.yellow,),
+                    Icon(Icons.star,color: Colors.yellow,),
+                    Icon(Icons.star,color: Colors.yellow,),
+                    Icon(Icons.star_border,color: Colors.grey,),
+                    Icon(Icons.star_border,color: Colors.grey,),
+                    Spacer(),
+                    Container(
+                      height: 30,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(16)
+                      ),
+                      child: Center(
+                        child: Text("148 Favorites"),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 24,),
+                Text(lipsum)
+              ],
+            ),
+          )
 
 
 
@@ -47,7 +90,6 @@ class _DetailPageState extends State<DetailPage> {
 
 
 
-          
         ],
       ),
     );
