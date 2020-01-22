f91fecd8da6a005686d37f88f9f199ab5a86371e
Dreamwalker <aristojeff@gmail.com>
2020-01-22 20:58:25 +0900

    Update detail_page.dart

--- lib/bookstore_app/detail_page.dart
+++ lib/bookstore_app/detail_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notebook_the_third_story/note_utils.dart';
 
-
 class DetailPage extends StatefulWidget {
   @override
   _DetailPageState createState() => _DetailPageState();
@@ -24,7 +23,8 @@ class _DetailPageState extends State<DetailPage> {
                 child: Container(
                   color: Colors.yellow,
                   child: Center(
-                    child: FlutterLogo(size: 240,
+                    child: FlutterLogo(
+                      size: 240,
                       style: FlutterLogoStyle.horizontal,
                     ),
                   ),
@@ -34,62 +34,64 @@ class _DetailPageState extends State<DetailPage> {
           ),
           Positioned(
             top: MediaQuery.of(context).size.height / 2.2,
-            left: 24,right: 16,
+            left: 24,
+            right: 16,
             bottom: 0,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text("Flutter Live Coding Book", style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22
-                ),),SizedBox(height: 8),
-                Text("Dreamwalker", style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16
-                ),),
+                Text(
+                  "Flutter Live Coding Book",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "Dreamwalker",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
                 SizedBox(height: 16),
                 Row(
                   children: <Widget>[
-                    Icon(Icons.star,color: Colors.yellow,),
-                    Icon(Icons.star,color: Colors.yellow,),
-                    Icon(Icons.star,color: Colors.yellow,),
-                    Icon(Icons.star_border,color: Colors.grey,),
-                    Icon(Icons.star_border,color: Colors.grey,),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                    Icon(
+                      Icons.star_border,
+                      color: Colors.grey,
+                    ),
+                    Icon(
+                      Icons.star_border,
+                      color: Colors.grey,
+                    ),
                     Spacer(),
                     Container(
                       height: 30,
                       width: 120,
                       decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(16)
-                      ),
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(16)),
                       child: Center(
                         child: Text("148 Favorites"),
                       ),
                     )
                   ],
                 ),
-                SizedBox(height: 24,),
+                SizedBox(
+                  height: 24,
+                ),
                 Text(lipsum)
               ],
             ),
           )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         ],
       ),
     );
