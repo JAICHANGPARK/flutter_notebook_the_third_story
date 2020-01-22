249785289346a0c860212a38be760e2b0e21fde3
Dreamwalker <aristojeff@gmail.com>
2020-01-22 21:08:41 +0900

    Create main_page.dart

---
+++ lib/profile_concept_app/main_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class ProfileConceptApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
