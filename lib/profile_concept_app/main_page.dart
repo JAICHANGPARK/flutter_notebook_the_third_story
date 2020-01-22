64ad6cd90907fd6b5a061ef52383ba939ea96583
Dreamwalker <aristojeff@gmail.com>
2020-01-22 21:11:39 +0900

    Update main_page.dart

--- lib/profile_concept_app/main_page.dart
+++ lib/profile_concept_app/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ProfileConceptApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,7 +15,38 @@ class MainPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height / 4.5,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.orangeAccent,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(24),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(24),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
