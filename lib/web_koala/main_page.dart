dc3cc4756a961143e7ee8853ad8a05f97133562a
Dreamwalker <aristojeff@gmail.com>
2020-01-25 15:08:58 +0900

    Adds

--- lib/web_koala/main_page.dart
+++ lib/web_koala/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class KoalaWebApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,11 +18,38 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Placeholder(),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Text("Flutter Web"),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Placeholder(),
+                )
+              ],
+            ),
 
+          )
+        ],
       ),
-      drawer: Drawer(),
     );
   }
 }
+
+
 
