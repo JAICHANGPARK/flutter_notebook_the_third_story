b5a31be36cac7da9a620286fd33ff893a76218f3
Dreamwalker <aristojeff@gmail.com>
2020-01-25 15:05:21 +0900

    Adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notebook_the_third_story/profile_concept_app/main_page.dart';
+import 'package:notebook_the_third_story/web_koala/main_page.dart';
 
-void main() => runApp(ProfileConceptApp());
+void main() => runApp(KoalaWebApp());
 
--- lib/web_koala/main_page.dart
+++ lib/web_koala/main_page.dart
@@ -22,6 +22,7 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(
 
       ),
+      drawer: Drawer(),
     );
   }
 }
