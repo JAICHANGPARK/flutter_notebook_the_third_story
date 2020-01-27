8ad7f37c2258d7eabf0918a9d97527f141489336
Dreamwalker <aristojeff@gmail.com>
2020-01-27 19:48:30 +0900

    Adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:notebook_the_third_story/default_page/main_page.dart';
+import 'package:notebook_the_third_story/cospalmer_app/main_page.dart';
 
-void main() => runApp(DefaultApp());
+
+void main() => runApp(OnlineShopApp());
 
