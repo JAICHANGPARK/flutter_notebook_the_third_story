887726288a8158cedd4b61f3ab7fd974b5e96991
Dreamwalker <aristojeff@gmail.com>
2020-01-29 21:13:44 +0900

    Update main_page.dart

--- lib/food_receipt_app/main_page.dart
+++ lib/food_receipt_app/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 class FoodReceiptsApp extends StatelessWidget {
@@ -18,7 +19,34 @@ class _FoodMainPageState extends State<FoodMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        
+        body: SingleChildScrollView(
+          child:
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16,
+              top: 24),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Icon(Icons.menu),
+                      Badge(
+                        child: Container(
+                          height: 38,
+                          width: 38,
+                          decoration: BoxDecoration(
+                            color: Colors.red
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
     );
   }
 }
