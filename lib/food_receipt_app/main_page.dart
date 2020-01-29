f70fcb2cd1db163f1daee0e82f54cd95cfeebfcd
Dreamwalker <aristojeff@gmail.com>
2020-01-29 21:16:06 +0900

    Update main_page.dart

--- lib/food_receipt_app/main_page.dart
+++ lib/food_receipt_app/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/note_utils.dart';
 
 class FoodReceiptsApp extends StatelessWidget {
   @override
@@ -20,35 +21,54 @@ class _FoodMainPageState extends State<FoodMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-          child:
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16,
-              top: 24),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Icon(Icons.menu),
-                      Badge(
-                        child: Container(
-                          height: 38,
-                          width: 38,
-                          decoration: BoxDecoration(
-                            color: Colors.red
-                          ),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 24),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(Icons.menu),
+                    Badge(
+                      badgeColor: Colors.yellow[700],
+                      child: Container(
+                        height: 38,
+                        width: 38,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(8),
+                            image: DecorationImage(
+                                image: NetworkImage(dreamwalkerImg))),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 120,
+                  child: Placeholder(),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "Good",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    Text(
+                      " evening",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
           ),
         ),
+      ),
     );
   }
 }
-
