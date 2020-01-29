2890ab930cc4abd29f14bb63a3a45a885e7b72f3
Dreamwalker <aristojeff@gmail.com>
2020-01-29 21:11:12 +0900

    Adds

--- lib/food_receipt_app/main_page.dart
+++ lib/food_receipt_app/main_page.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
-
 class FoodReceiptsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: FoodMainPage(),
     );
   }
 }
+
+class FoodMainPage extends StatefulWidget {
+  @override
+  _FoodMainPageState createState() => _FoodMainPageState();
+}
+
+class _FoodMainPageState extends State<FoodMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:notebook_the_third_story/food_receipt_app/main_page.dart';
-
-
 void main() => runApp(FoodReceiptsApp());
 
