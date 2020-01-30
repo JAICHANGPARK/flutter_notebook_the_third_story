f86f6e510287f24dd9d3f6afd050c06408cd7bb5
Dreamwalker <aristojeff@gmail.com>
2020-01-30 22:05:47 +0900

    Update food_info_page.dart

--- lib/food_receipt_app/food_info_page.dart
+++ lib/food_receipt_app/food_info_page.dart
@@ -31,6 +31,69 @@ class _FoodInfoPageState extends State<FoodInfoPage> {
               ),
             ),
           ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 180,
+              padding: EdgeInsets.only(left: 24, top: 48, right: 24, bottom: 32),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(24)
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("3 pieces"),
+                      Text("1032cal"),
+                      Text("20-30min"),
+                    ],
+                  ),
+
+                  Container(
+                    height: 60,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 5,
+                          child: Container(
+                            margin: EdgeInsets.only(right: 16),
+                            decoration: BoxDecoration(
+                              color: Colors.yellow,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Center(
+                              child: Text("Start cooking",style: TextStyle(
+                                fontSize: 20
+                              ),),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all()
+                            ),
+                            child: Center(
+                              child: Icon(Icons.favorite),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+
+                ],
+              ),
+            ),
+          )
 
         ],
         ),
