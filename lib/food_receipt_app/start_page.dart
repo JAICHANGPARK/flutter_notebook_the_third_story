de87a6df28f62a14972527b62eddf6bc0c00dbe8
Dreamwalker <aristojeff@gmail.com>
2020-01-31 22:35:27 +0900

    Update start_page.dart

--- lib/food_receipt_app/start_page.dart
+++ lib/food_receipt_app/start_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/food_receipt_app/main_page.dart';
 
 class StartPage extends StatelessWidget {
   @override
@@ -16,7 +17,7 @@ class StartPage extends StatelessWidget {
                 image: DecorationImage(
                   image: NetworkImage("https://cdn.pixabay.com/photo/2017/09/28/18/13/bread-2796393_960_720.jpg"),
                   fit: BoxFit.cover,
-                  colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.45),
+                  colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.6),
                   BlendMode.darken)
                 )
               ),
@@ -24,11 +25,46 @@ class StartPage extends StatelessWidget {
           ),
           Positioned(
             left: 24,
-            top: 130,
+            top: 180,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-
+                Text("Welcome", style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24
+                ),),
+                Text("to cook book", style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24
+                ),)
               ],
+            ),
+          ),
+          Positioned(
+            left: 24,
+            right: 24,
+            bottom: 24,
+            child: InkWell(
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (_)=>FoodMainPage()
+                ));
+              },
+              child: Container(
+
+                height: 64,
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Center(
+                  child: Text("Start now",style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20
+                  ),),
+                ),
+              ),
             ),
           )
 
