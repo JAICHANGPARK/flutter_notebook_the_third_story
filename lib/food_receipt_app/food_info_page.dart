0a38422ea5a98547e95bcca788b042cba444b7d2
Dreamwalker <aristojeff@gmail.com>
2020-01-31 22:25:06 +0900

    Adds

--- lib/food_receipt_app/food_info_page.dart
+++ lib/food_receipt_app/food_info_page.dart
@@ -1,4 +1,6 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:notebook_the_third_story/note_utils.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class FoodInfoPage extends StatefulWidget {
@@ -12,10 +14,56 @@ class _FoodInfoPageState extends State<FoodInfoPage> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SlidingUpPanel(
+        body: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 16,
+              right: 24,
+              top: 32,
+              child: Container(
+                height: 64,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: (){},
+                      color: Colors.white,
+                    ),
+                    Badge(
+                      badgeColor: Colors.yellow[700],
+                      child: Container(
+                        height: 38,
+                        width: 38,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(8),
+                            image: DecorationImage(
+                                image: NetworkImage(dreamwalkerImg))),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 24,
+              right: 24,
+              top: 320,
+              child: Column(
+                children: <Widget>[
+                  Text("The best"),
+                  Text("chicken tinga tacos")
+                ],
+              ),
+            )
+          ],
+        ),
         defaultPanelState: PanelState.OPEN,
         borderRadius: BorderRadius.circular(24),
         panel: Stack(
           children: <Widget>[
+
             Positioned(
               left: 0,
               right: 0,
