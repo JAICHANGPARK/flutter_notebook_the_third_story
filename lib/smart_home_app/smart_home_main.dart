2f0e814a8bbfac2487fa0cfadd6a4f37759f5a4b
Dreamwalker <aristojeff@gmail.com>
2020-02-01 19:34:09 +0900

    Create smart_home_main.dart

---
+++ lib/smart_home_app/smart_home_main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class SmartHomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SMHome(),
+    );
+  }
+}
+class SMHome extends StatefulWidget {
+  @override
+  _SMHomeState createState() => _SMHomeState();
+}
+
+class _SMHomeState extends State<SMHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
