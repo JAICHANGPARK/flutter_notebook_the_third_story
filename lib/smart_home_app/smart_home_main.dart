855f27d9208d4b5757b644926ed86295190f476d
Dreamwalker <aristojeff@gmail.com>
2020-02-02 20:11:16 +0900

    Update smart_home_main.dart

--- lib/smart_home_app/smart_home_main.dart
+++ lib/smart_home_app/smart_home_main.dart
@@ -27,25 +27,71 @@ class _SMHomeState extends State<SMHome> {
         body: Stack(
       children: <Widget>[
         Positioned(
+          left: 24,
+          right: 24,
+          top: 72,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text.rich(TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Hello, ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w200,
+                      fontSize: 32
+                    )
+                  ),
+                  TextSpan(
+                    text: "Dreamwalker!",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w300,
+                    )
+                  )
+                ]
+              )),
+              SizedBox(height: 7,),
+              Text("Aniting I can help you with?",style: TextStyle(
+                color: Colors.black.withOpacity(0.4),
+                fontWeight: FontWeight.w300
+              ),),
+              
+            ],
+          ),
+        ),
+        Positioned(
           bottom: 0,
           right: 0,
           left: 0,
           child: Container(
             height: 160,
+            margin: EdgeInsets.only(top: 16),
             decoration: BoxDecoration(
 //                color: Colors.grey.shade400,
                 gradient: LinearGradient(
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
                     colors: <Color>[
-                      Colors.grey.shade50,
+                      Colors.grey.shade100,
                       Colors.grey.shade200,
-                      Colors.grey.shade200,
+                      Colors.grey.shade300,
+                      Colors.blueGrey.shade200,
                     ]),
                 borderRadius: BorderRadius.only(
                   topRight: Radius.circular(48),
                   topLeft: Radius.circular(48),
-                )),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 16,
+                      offset: Offset(-24, -8),
+                      color: Colors.white),
+//                  BoxShadow(
+//                      blurRadius: 16,
+//                      offset: widget.blurOffset,
+//                      color: color.mix(Colors.black, 0.3))
+                ]),
           ),
         ),
       ],
