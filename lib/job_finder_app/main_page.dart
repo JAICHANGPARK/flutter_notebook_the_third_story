3e0b4a09e225b2741b4a1a941d6e433ce49d4c74
Dreamwalker <aristojeff@gmail.com>
2020-02-04 21:15:19 +0900

    Update main_page.dart

--- lib/job_finder_app/main_page.dart
+++ lib/job_finder_app/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class JobFinderApp extends StatelessWidget {
@@ -6,11 +7,12 @@ class JobFinderApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: "/",
       routes: {
-        "/" : (context)=>JopFinderMainPage(),
+        "/": (context) => JopFinderMainPage(),
       },
     );
   }
 }
+
 class JopFinderMainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
@@ -24,51 +26,65 @@ class _MainPageState extends State<JopFinderMainPage> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        iconTheme: IconThemeData(
-          color: Colors.black
-        ),
+        iconTheme: IconThemeData(color: Colors.black),
         leading: IconButton(
           icon: Icon(Icons.menu),
-          onPressed: () {  },
+          onPressed: () {},
         ),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.search), onPressed: () {  },
+            icon: Icon(Icons.search),
+            onPressed: () {},
           ),
           IconButton(
-            icon: Icon(Icons.tune,), onPressed: () {  },
+            icon: Icon(
+              Icons.tune,
+            ),
+            onPressed: () {},
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 32,
+            ),
+            Text("Designer"),
+            Text("Job"),
+            SizedBox(
+              height: 48,
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  height: 34,
+                  width: 108,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("New York"),
+                      IconButton(
+                        icon: Icon(
+                          Icons.clear,
+                        ),
+                        onPressed: () {},
+                        iconSize: 16,
+                      )
+                    ],
+                  ),
+                ),
+                Container()
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
